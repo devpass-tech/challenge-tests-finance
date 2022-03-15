1e842a900b6919754ac0ea1ad6f18796e18d37c3
bruna.drago <bruna_drago@hotmail.com>
2022-03-14 23:01:01 -0300

    Add unit test to insideNavigation method

---
+++ solutions/devsprint-caio-santos-2/FinanceAppTests/InsideNavigationTests.swift
@@ -0,0 +1,17 @@
+import XCTest
+
+@testable import FinanceApp
+
+class InsideNavigationTests: XCTestCase {
+
+    func test_insideNavigation_shouldReturn_correctConfiguration() {
+        let dummyController = UIViewController()
+        
+        let sut = dummyController.insideNavigationController()
+        
+        XCTAssertNotNil(sut)
+        XCTAssertEqual(sut.modalPresentationStyle, .formSheet)
+        XCTAssertEqual(sut.viewControllers.count, 1)
+        XCTAssertEqual(sut.viewControllers[0], dummyController)
+    }
+}
