aacdf8a017efac9bbca4a1b728301809e3e97000
bruna.drago <bruna_drago@hotmail.com>
2022-03-15 22:02:58 -0300

    Refactor insideNavigation unit tests

--- solutions/devsprint-caio-santos-2/FinanceAppTests/InsideNavigationTests.swift
+++ solutions/devsprint-caio-santos-2/FinanceAppTests/InsideNavigationTests.swift
@@ -2,15 +2,25 @@ import XCTest
 @testable import FinanceApp
 
 class InsideNavigationTests: XCTestCase {
+    private let dummyController = UIViewController()
 
-    func test_insideNavigation_shouldReturn_CorrectConfiguration() {
-        let dummyController = UIViewController()
-        
+    func test_insideNavigation_should_NotBeNil() {
         let sut = dummyController.insideNavigationController()
-        
         XCTAssertNotNil(sut)
+    }
+    
+    func test_insideNavigation_shouldPresent_withModalFormSheetPresentationStyle() {
+        let sut = dummyController.insideNavigationController()
         XCTAssertEqual(sut.modalPresentationStyle, .formSheet)
+    }
+    
+    func test_insideNavigation_shouldContains_viewController() {
+        let sut = dummyController.insideNavigationController()
+        XCTAssertEqual(sut.viewControllers.first, dummyController)
+    }
+    
+    func test_insideNavigation_shouldHave_oneViewController() {
+        let sut = dummyController.insideNavigationController()
         XCTAssertEqual(sut.viewControllers.count, 1)
-        XCTAssertEqual(sut.viewControllers[0], dummyController)
     }
 }
