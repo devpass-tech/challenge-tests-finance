ece5db20a0aba462ea9c3cb03aedb132d3884b15
Pedro Francisco Tres <52545592+pedrotres@users.noreply.github.com>
2022-03-15 23:47:30 -0300

    Refactor tests

--- solutions/devsprint-caio-santos-2/FinanceAppTests/SnapshotTests/HomeHeaderViewSnapshotTests.swift
+++ solutions/devsprint-caio-santos-2/FinanceAppTests/SnapshotTests/HomeHeaderViewSnapshotTests.swift
@@ -13,7 +13,7 @@ class HomeHeaderViewSnapshotTests: XCTestCase {
     let size = CGSize(width: 414, height: 149)
     let isRecord = false
     
-    func test_homeHeaderView_whenSnapshot_shouldSucceed() {
+    func test_HomeHeaderView_allFields() {
         let configuration = HomeHeaderViewConfiguration(label: "$15,459.27", savingsValueLabel: "$100.00", spendingValueLabel: "$100.00")
         let homeHeaderView = HomeHeaderView()
         homeHeaderView.backgroundColor = .white
@@ -22,7 +22,7 @@ class HomeHeaderViewSnapshotTests: XCTestCase {
         assertSnapshot(matching: homeHeaderView, as: .image(size: size), record: isRecord)
     }
     
-    func test_homeHeaderView_whenSnapshot_noLabel() {
+    func test_HomeHeaderView_should_emptyName_when_noLabel() {
         let configuration = HomeHeaderViewConfiguration(label: "", savingsValueLabel: "$100.00", spendingValueLabel: "$100.00")
         let homeHeaderView = HomeHeaderView()
         homeHeaderView.backgroundColor = .white
@@ -31,7 +31,7 @@ class HomeHeaderViewSnapshotTests: XCTestCase {
         assertSnapshot(matching: homeHeaderView, as: .image(size: size), record: isRecord)
     }
     
-    func test_homeHeaderView_whenSnapshot_noSavingsValue() {
+    func test_HomeHeaderView_should_emptyName_when_noSavingsValue() {
         let configuration = HomeHeaderViewConfiguration(label: "$15,459.27", savingsValueLabel: "", spendingValueLabel: "$100.00")
         let homeHeaderView = HomeHeaderView()
         homeHeaderView.backgroundColor = .white
@@ -40,7 +40,7 @@ class HomeHeaderViewSnapshotTests: XCTestCase {
         assertSnapshot(matching: homeHeaderView, as: .image(size: size), record: isRecord)
     }
     
-    func test_homeHeaderView_whenSnapshot_noSpedingValue() {
+    func test_HomeHeaderView_should_emptyName_when_noSpedingValue() {
         let configuration = HomeHeaderViewConfiguration(label: "$15,459.27", savingsValueLabel: "$100.00", spendingValueLabel: "")
         let homeHeaderView = HomeHeaderView()
         homeHeaderView.backgroundColor = .white
@@ -49,7 +49,7 @@ class HomeHeaderViewSnapshotTests: XCTestCase {
         assertSnapshot(matching: homeHeaderView, as: .image(size: size), record: isRecord)
     }
     
-    func test_homeHeaderView_whenSnapshot_longLabel() {
+    func test_HomeHeaderView_should_label_when_longLabel() {
         let configuration = HomeHeaderViewConfiguration(label: "$15,459,567.27", savingsValueLabel: "$100.00", spendingValueLabel: "$100.00")
         let homeHeaderView = HomeHeaderView()
         homeHeaderView.backgroundColor = .white
@@ -58,7 +58,7 @@ class HomeHeaderViewSnapshotTests: XCTestCase {
         assertSnapshot(matching: homeHeaderView, as: .image(size: size), record: isRecord)
     }
     
-    func test_homeHeaderView_whenSnapshot_longSavingsValue() {
+    func test_HomeHeaderView_should_savingsValue_when_longSavingsValue() {
         let configuration = HomeHeaderViewConfiguration(label: "$15,459.27", savingsValueLabel: "$10,000,000.00", spendingValueLabel: "$100.00")
         let homeHeaderView = HomeHeaderView()
         homeHeaderView.backgroundColor = .white
@@ -67,7 +67,7 @@ class HomeHeaderViewSnapshotTests: XCTestCase {
         assertSnapshot(matching: homeHeaderView, as: .image(size: size), record: isRecord)
     }
     
-    func test_homeHeaderView_whenSnapshot_longSpedingValue() {
+    func test_HomeHeaderView_should_spedingValue_when_longSpedingValue() {
         let configuration = HomeHeaderViewConfiguration(label: "$15,459.27", savingsValueLabel: "$100.00", spendingValueLabel: "$44,000,000.00")
         let homeHeaderView = HomeHeaderView()
         homeHeaderView.backgroundColor = .white
