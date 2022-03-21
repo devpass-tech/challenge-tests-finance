b7218f02602752e2edce9c9f2bb9ac12941192d9
Pedro Francisco Tres <52545592+pedrotres@users.noreply.github.com>
2022-03-21 11:44:49 -0300

    Separate class configuration

--- solutions/devsprint-caio-santos-2/FinanceApp/Components/ActivityCellView/ActivityCellView.swift
+++ solutions/devsprint-caio-santos-2/FinanceApp/Components/ActivityCellView/ActivityCellView.swift
@@ -8,17 +8,17 @@
 import UIKit
 
 class ActivityCellView: UITableViewCell {
-
-   private var mainStackView: UIStackView = {
-       let stack = UIStackView(frame: .zero)
-       stack.translatesAutoresizingMaskIntoConstraints = false
-       stack.spacing = 8
-       stack.alignment = .center
-       stack.isLayoutMarginsRelativeArrangement = true
-       stack.layoutMargins = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 0)
-       return stack
+    
+    private var mainStackView: UIStackView = {
+        let stack = UIStackView(frame: .zero)
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.spacing = 8
+        stack.alignment = .center
+        stack.isLayoutMarginsRelativeArrangement = true
+        stack.layoutMargins = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 0)
+        return stack
     }()
-
+    
     private var labelsStackView: UIStackView = {
         let stack = UIStackView(frame: .zero)
         stack.translatesAutoresizingMaskIntoConstraints = false
@@ -26,7 +26,7 @@ class ActivityCellView: UITableViewCell {
         stack.spacing = 8
         return stack
     }()
-
+    
     lazy var categoryImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
@@ -36,7 +36,7 @@ class ActivityCellView: UITableViewCell {
         imageView.tintColor = .systemPurple
         return imageView
     }()
-
+    
     lazy var activityNameLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -44,7 +44,7 @@ class ActivityCellView: UITableViewCell {
         label.text = "Mall"
         return label
     }()
-
+    
     lazy var activityInfoLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -53,44 +53,49 @@ class ActivityCellView: UITableViewCell {
         label.text = "$100.00 • 8:57 AM"
         return label
     }()
-
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         self.accessoryType = .disclosureIndicator
-
+        
         addSubviews()
         configureConstraints()
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
 
 extension ActivityCellView {
-
+    
     func addSubviews() {
-
+        
         addSubview(mainStackView)
         mainStackView.addArrangedSubview(categoryImageView)
         mainStackView.addArrangedSubview(labelsStackView)
-
+        
         labelsStackView.addArrangedSubview(activityNameLabel)
         labelsStackView.addArrangedSubview(activityInfoLabel)
     }
-
+    
     func configureConstraints() {
-
+        
         NSLayoutConstraint.activate([
             mainStackView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
             mainStackView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor),
             mainStackView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
             mainStackView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor),
-
+            
             self.categoryImageView.widthAnchor.constraint(equalToConstant: 50),
             self.categoryImageView.heightAnchor.constraint(equalToConstant: 50),
-
         ])
+    }
+    
+    func updateView(with configuration: ActivityCellViewConfiguration) {
+        categoryImageView.image = UIImage(named: configuration.image)
+        activityNameLabel.text = configuration.name
+        activityInfoLabel.text = configuration.information
     }
 }
 
