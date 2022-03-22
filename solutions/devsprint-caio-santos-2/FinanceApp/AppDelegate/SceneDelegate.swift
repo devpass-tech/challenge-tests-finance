85aee36bc5353961d905fd3738a0cd8a1c866dc7
Caio C. Lopes <c.cezar.lopes@gmail.com>
2022-03-21 22:06:05 -0300

    Adjusting first navigation screen.

--- solutions/devsprint-caio-santos-2/FinanceApp/AppDelegate/SceneDelegate.swift
+++ solutions/devsprint-caio-santos-2/FinanceApp/AppDelegate/SceneDelegate.swift
@@ -17,10 +17,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
         self.window = UIWindow(frame: UIScreen.main.bounds)
-//        self.window?.rootViewController = UINavigationController(rootViewController: TabBarController())
+        self.window?.rootViewController = UINavigationController(rootViewController: TabBarController())
         
-        let loginViewController = LoginConfigurator().createModule()
-        self.window?.rootViewController = UINavigationController(rootViewController: loginViewController)
+//        let loginViewController = LoginConfigurator().createModule()
+//        self.window?.rootViewController = UINavigationController(rootViewController: loginViewController)
         self.window?.windowScene = windowScene
         self.window?.makeKeyAndVisible()
     }
