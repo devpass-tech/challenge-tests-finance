4d3b97eaed29f679fac757f636ec5f48ca4d6aec
Will Policiano <w.policiano@gmail.com>
2022-03-22 18:20:23 -0500

    Create Core module

---
+++ solutions/devsprint-willian-policiano-1/Core/Core.h
@@ -0,0 +1,18 @@
+//
+//  Core.h
+//  Core
+//
+//  Created by Willian Policiano on 22/03/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Core.
+FOUNDATION_EXPORT double CoreVersionNumber;
+
+//! Project version string for Core.
+FOUNDATION_EXPORT const unsigned char CoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Core/PublicHeader.h>
+
+
