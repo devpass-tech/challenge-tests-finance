f92ad33d8ae8853a5ac8dca3eacab7e6c97ce47c
Will Policiano <w.policiano@gmail.com>
2022-03-24 01:41:31 -0500

    Deliver error when there is no Spending

--- solutions/devsprint-willian-policiano-1/CoreTests/HomeServiceTests.swift
+++ solutions/devsprint-willian-policiano-1/CoreTests/HomeServiceTests.swift
@@ -133,6 +133,21 @@ class HomeServiceTests: XCTestCase {
         XCTAssertEqual(actualResult?.error as? ServiceError, .invalidData)
     }
 
+    func test_failsWhenThereIsNoSpending() throws {
+        let invalidJson = Data("""
+        {
+            "balance_price": "15459.27",
+            "svgs": 1000.0
+        }
+        """.utf8)
+
+        let actualResult = result(when: { httpClient in
+            httpClient.completeWithSuccess((200, invalidJson))
+        })
+
+        XCTAssertEqual(actualResult?.error as? ServiceError, .invalidData)
+    }
+
     // MARK: Helpers
 
     private let jsonData = Data("""
