4034ce342d096a697c5fe6429f8ef3c55f6f94e3
bruna.drago <bruna_drago@hotmail.com>
2022-03-24 19:51:06 -0300

    Add Profile UI tests

---
+++ solutions/devsprint-caio-santos-2/FinanceAppUITests/ProfileUITests.swift
@@ -0,0 +1,20 @@
+import XCTest
+
+class ProfileUITests: XCTestCase {
+
+    func test_navigation_to_ProfileScreen() throws {
+        let app = XCUIApplication()
+        app.launch()
+        
+        let navigationBar = app.navigationBars["FinanceApp.HomeView"]
+        let profileButton = navigationBar.buttons["Profile"]
+        
+        profileButton.tap()
+        
+        let userName = app.staticTexts["User Name"]
+        let bankName = app.staticTexts["Devpass Bank"]
+        XCTAssert(userName.waitForExistence(timeout: 5))
+        XCTAssert(bankName.waitForExistence(timeout: 5))
+    }
+
+}
