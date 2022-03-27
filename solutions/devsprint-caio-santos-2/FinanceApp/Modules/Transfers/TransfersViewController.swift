4a97e7dee5bb09159b77b2d135a0c8e561005719
Pedro Francisco Tres <52545592+pedrotres@users.noreply.github.com>
2022-03-26 22:10:40 -0300

    added action on keyboard return key

--- solutions/devsprint-caio-santos-2/FinanceApp/Modules/Transfers/TransfersViewController.swift
+++ solutions/devsprint-caio-santos-2/FinanceApp/Modules/Transfers/TransfersViewController.swift
@@ -7,31 +7,36 @@
 
 import UIKit
 
-class TransfersViewController: UIViewController {
-
+class TransfersViewController: UIViewController, UITextFieldDelegate {
+    
     lazy var transferView: TransfersView = {
-
+        
         let transferView = TransfersView()
         transferView.delegate = self
+        transferView.amountTextField.delegate = self
         return transferView
     }()
-
+    
     override func loadView() {
         self.view = transferView
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        transferView.amountTextField.resignFirstResponder()
     }
 }
 
 extension TransfersViewController: TransferViewDelegate {
-
+    
     func didPressChooseContactButton() {
-
+        
         let contactViewController = ContactListRouter.createModule()
-
+        
         self.present(contactViewController, animated: true)
     }
-
+    
     func didPressTransferButton() {
-
+        
         let navigationController = UINavigationController(rootViewController: ConfirmationViewController())
         self.present(navigationController, animated: true)
     }
