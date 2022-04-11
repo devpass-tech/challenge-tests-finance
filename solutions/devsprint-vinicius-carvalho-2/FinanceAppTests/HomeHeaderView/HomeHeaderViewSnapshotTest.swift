d35e69672808533f86e4a0be1f4a604848e84ecc
Henrique Cora <henrique@cora.com.br>
2022-04-11 18:31:06 -0300

    make snapshot test from home header view

---
+++ solutions/devsprint-vinicius-carvalho-2/FinanceAppTests/HomeHeaderView/HomeHeaderViewSnapshotTest.swift
@@ -0,0 +1,19 @@
+//
+//  HomeHeaderViewSnapshotTest.swift
+//  FinanceAppTests
+//
+//  Created by Henrique Augusto on 11/04/22.
+//
+
+import XCTest
+import SnapshotTesting
+
+@testable import FinanceApp
+
+class HomeHeaderViewSnapshotTest: XCTestCase {
+    func test_HomeHeaderView() {
+        let sut: HomeHeaderView = HomeHeaderView()
+        
+        assertSnapshot(matching: sut, as: .image)
+    }
+}
