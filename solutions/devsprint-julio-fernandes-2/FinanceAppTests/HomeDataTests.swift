24971fb6c91e9de1fafdcbbdd5e676f35ae01ae3
Pedro Henrique Martins Barbosa <pbarbosa@ciandt.com>
2022-04-20 09:05:22 -0300

    feat: add unitary tests for formattedInfo

---
+++ solutions/devsprint-julio-fernandes-2/FinanceAppTests/HomeDataTests.swift
@@ -0,0 +1,23 @@
+//
+//  HomeDataTests.swift
+//  FinanceAppTests
+//
+//  Created by Pedro Henrique Martins Barbosa on 20/04/22.
+//
+
+import XCTest
+@testable import FinanceApp
+
+class HomeDataTests: XCTestCase {
+    
+    var activity = Activity(name: "atividade", price: 1000.0, time: "50")
+    
+    func test_formattedInfo_shouldReturnValidFormat() {
+        let formattedStringValue = "$\(1000.0) • \("50")"
+        XCTAssertEqual(formattedStringValue, activity.formattedInfo())
+    }
+
+    func test_formattedInfo_shouldReturnNotNil() {
+        XCTAssertNotNil(activity.formattedInfo())
+    }
+}
