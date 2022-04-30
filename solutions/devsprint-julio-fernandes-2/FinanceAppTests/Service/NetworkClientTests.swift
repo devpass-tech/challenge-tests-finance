6a36ce7b9bda27a066aaccb4d1a50ff138e4cca1
Julio Fernandes <julio.fernandes7@gmail.com>
2022-04-30 12:26:38 -0300

    adicionando testes de interface

---
+++ solutions/devsprint-julio-fernandes-2/FinanceAppTests/Service/NetworkClientTests.swift
@@ -0,0 +1,43 @@
+//
+//  NetworkClientTests.swift
+//  FinanceAppTests
+//
+//  Created by Julio Fernandes on 30/04/22.
+//
+
+import XCTest
+@testable import FinanceApp
+
+final class NetworkClientTests: XCTestCase {
+
+    var sut: NetworkClient?
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        sut = NetworkClient()
+    }
+
+    override func tearDownWithError() throws {
+        sut = nil
+        try super.tearDownWithError()
+    }
+
+    func test_performRequest_returned_success() throws {
+        guard let url = makeURL(file: "localSuccessData") else {
+            XCTFail("is not valid URL")
+            return
+        }
+        
+        sut?.performRequest(with: url) { data in
+            XCTAssertNotNil(data)
+        }
+    }
+    
+    private func makeURL(file: String) -> URL? {
+        guard let bundle = Bundle(for: FinanceService2Tests.self).url(forResource: file, withExtension: "json") else {
+            return nil
+        }
+        return bundle
+    }
+
+}
---
+++ solutions/devsprint-julio-fernandes-2/FinanceAppUITests/ActivityDetailsUITests.swift
@@ -0,0 +1,24 @@
+//
+//  ActivityDetailsUITests.swift
+//  FinanceAppUITests
+//
+//  Created by Julio Fernandes on 30/04/22.
+//
+
+import XCTest
+
+class ActivityDetailsUITests: XCTestCase {
+    func test_navigation_activityList_to_activityDetails() {
+        let app = XCUIApplication()
+        app.launch()
+        
+        let table = app.tables.element(boundBy: 0)
+        table.tap()
+
+        let mallLabel = app.staticTexts["Mall"]
+        let shoppingLabel = app.staticTexts["Shopping"]
+
+        XCTAssert(mallLabel.exists)
+        XCTAssert(shoppingLabel.exists)
+    }
+}
