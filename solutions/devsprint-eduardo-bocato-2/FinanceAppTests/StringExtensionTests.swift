b6222d61d95e91aca439604f4fa36ae8a6a1b3bf
Lucas Eduardo Schlögl <leschlogl@users.noreply.github.com>
2022-05-05 17:21:13 -0300

    fixes test names

--- solutions/devsprint-eduardo-bocato-2/FinanceAppTests/StringExtensionTests.swift
+++ solutions/devsprint-eduardo-bocato-2/FinanceAppTests/StringExtensionTests.swift
@@ -2,30 +2,30 @@ import XCTest
 @testable import FinanceApp
 
 final class StringExtensionTests: XCTestCase {
-    func test_whenStringHasMidSpace_shouldRemoveSpace() throws {
-        //Given
+    func test_removingSpaces_whenStringHasMidSpace_shouldRemoveSpace() throws {
+        // Given
         let sut: String = "some text"
-        //When
+        // When
         let result = sut.removingSpaces()
-        //Then
+        // Then
         XCTAssertEqual(result, "sometext")
     }
 
-    func test_whenStringHasTailSpace_shouldRemoveSpace() throws {
-        //Given
+    func test_removingSpaces_whenStringHasTailSpace_shouldRemoveSpace() throws {
+        // Given
         let sut: String = "sometext "
-        //When
+        // When
         let result = sut.removingSpaces()
-        //Then
+        // Then
         XCTAssertEqual(result, "sometext")
     }
 
-    func test_whenStringHasTrailSpace_shouldRemoveSpace() throws {
-        //Given
+    func test_removingSpaces_whenStringHasTrailSpace_shouldRemoveSpace() throws {
+        // Given
         let sut: String = " sometext"
-        //When
+        // When
         let result = sut.removingSpaces()
-        //Then
+        // Then
         XCTAssertEqual(result, "sometext")
     }
 }
