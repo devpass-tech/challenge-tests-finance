5213b1a37698b715245c1703f3e0415e7ba3f536
Matheus Lenke Coutinho <matheuslenke@gmail.com>
2022-05-11 18:22:48 -0300

    refactor: Removing redundant tests

--- solutions/devsprint-attekita-3/FinanceAppTests/Extensions/UITableViewCellExtentionsTests.swift
+++ solutions/devsprint-attekita-3/FinanceAppTests/Extensions/UITableViewCellExtentionsTests.swift
@@ -10,29 +10,10 @@ import XCTest
 
 class UITableViewCellExtensionsTests: XCTestCase {
 
-    
-    func testClassIdentifierMethod_WhenActivityCellView_ShouldReturnIdentifier() throws {
-        let identifier = ActivityCellView.classIdentifier()
-        
-        XCTAssertEqual(identifier, "ActivityCellView")
-    }
-    
-    func testClassIdentifierMethod_WhenContactCellView_ShouldReturnIdentifier() throws {
-        let identifier = ContactCellView.classIdentifier()
-        
-        XCTAssertEqual(identifier, "ContactCellView")
-    }
-    
     func testClassIdentifierMethod_WhenUITableViewCell_ShouldReturnIdentifier() throws {
         let identifier = UITableViewCell.classIdentifier()
         
         XCTAssertEqual(identifier, "UITableViewCell")
-    }
-    
-    func testClassIdentifierMethod_WhenUITableViewCell_ShouldBeNotNil() {
-        let identifier = UITableViewCell.classIdentifier()
-        
-        XCTAssertNotNil(identifier)
     }
 
 }
