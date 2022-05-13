183f2337b5d90edaacab6a4322365db827b65a0f
Rodrigo Lemos <rodlemos.d@gmail.com>
2022-05-13 18:27:06 -0400

    Implement tests fot fetch home data

--- solutions/devsprint-attekita-3/FinanceAppTests/Service/FinanceServiceTests.swift
+++ solutions/devsprint-attekita-3/FinanceAppTests/Service/FinanceServiceTests.swift
@@ -53,4 +53,43 @@ final class FinanceServiceTests: XCTestCase {
         XCTAssertEqual(result?.price, 100.0, "the received price property fails becuase is not equal to json payload.")
         XCTAssertEqual(result?.time, "8:57 AM", "the received time property fails becuase is not equal to json payload.")
     }
+
+    func testFetchHomeDataMethod_WhenJSONDecoded_ShouldBeNotNil() {
+        // Given
+        let expectations = self.expectation(description: "WhenJSONDecoded_ShouldBeNotNil")
+        var result: HomeData? = nil
+
+        // When
+        sut.fetchHomeData { response in
+            result = response
+            expectations.fulfill()
+        }
+
+        // Then
+        waitForExpectations(timeout: 3.0)
+
+        XCTAssertNotNil(result, "fails because result is nil")
+    }
+
+    func testFetchHomeDataMethod_WhenJSONDecoded_ShouldHaveCorrectInformations() {
+        // Given
+        let expectations = self.expectation(description: "WhenJSONDecoded_ShouldHaveCorrectInformations")
+        var result: HomeData? = nil
+
+        // When
+        sut.fetchHomeData { response in
+            result = response
+            expectations.fulfill()
+        }
+
+        // Then
+        waitForExpectations(timeout: 3.0)
+
+        XCTAssertEqual(result?.balance, 15459.27)
+        XCTAssertEqual(result?.savings, 1000.0)
+        XCTAssertEqual(result?.spending, 500.0)
+        XCTAssertEqual(result?.activity[0].name, "Mall")
+        XCTAssertEqual(result?.activity[0].price, 100.0)
+        XCTAssertEqual(result?.activity[0].time, "8:57 AM")
+    }
 }
