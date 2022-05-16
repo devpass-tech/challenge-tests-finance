f608c27090304e2ef67f31af1599d6a1dab699e5
Tatiana Rico <tatianarico1702@gmail.com>
2022-05-16 18:59:17 -0300

    add private var, ajuste da identação

--- solutions/devsprint-attekita-3/FinanceAppTests/Service/HomeDataTest.swift
+++ solutions/devsprint-attekita-3/FinanceAppTests/Service/HomeDataTest.swift
@@ -9,32 +9,32 @@ import XCTest
 @testable import FinanceApp
 
 class HomeDataTest: XCTestCase {
-    var sut: Activity?
-       
-       override func setUpWithError() throws {
-           try super.setUpWithError()
-           
-           sut = Activity(name: "Mercado", price: 500, time: "60")
-       }
-
-       override func tearDownWithError() throws {
-           sut = nil
-           
-           try super.tearDownWithError()
-       }
-       
-       func test_formattedInfo_shouldReturnValidFormat() {
-           let formattedStringValue: String = "$\(500.0) • \("60")"
-              XCTAssertEqual(formattedStringValue, sut?.formattedInfo())
-          }
-      
-          func test_formattedInfo_shouldReturnNotNil() {
-              XCTAssertNotNil(sut?.formattedInfo())
-          }
+   private var sut: Activity?
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        
+        sut = Activity(name: "Mercado", price: 500, time: "60")
+    }
+    
+    override func tearDownWithError() throws {
+        sut = nil
+        
+        try super.tearDownWithError()
+    }
+    
+    func test_formattedInfo_shouldReturnValidFormat() {
+        let formattedStringValue: String = "$\(500.0) • \("60")"
+        XCTAssertEqual(formattedStringValue, sut?.formattedInfo())
+    }
+    
+    func test_formattedInfo_shouldReturnNotNil() {
+        XCTAssertNotNil(sut?.formattedInfo())
+    }
     
     func test_formattedInfo_shouldReturnEmpty() {
         let formattedStringValue = Activity(name: "", price: 0.0, time: "")
-    
+        
         XCTAssertEqual(formattedStringValue.formattedInfo(), "$0.0 • ")
     }
-   }
+}
