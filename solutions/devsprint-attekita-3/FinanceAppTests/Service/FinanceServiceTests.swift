d5e3b6feef2c422e5eb32f1c34d08aecb3ec1a72
Rodrigo Lemos <rodlemos.d@gmail.com>
2022-05-17 16:22:45 -0400

    ident file

--- solutions/devsprint-attekita-3/FinanceAppTests/Service/FinanceServiceTests.swift
+++ solutions/devsprint-attekita-3/FinanceAppTests/Service/FinanceServiceTests.swift
@@ -94,45 +94,46 @@ final class FinanceServiceTests: XCTestCase {
         XCTAssertEqual(result?.activity[0].name, "Mall")
         XCTAssertEqual(result?.activity[0].price, 100.0)
         XCTAssertEqual(result?.activity[0].time, "8:57 AM")
-    
-    func test_WhenJSONDecode_ShouldHaveReturnCorrectInformations() {
-        
-        // Given
-        let expectations: XCTestExpectation = expectation(description: "ShouldHaveCorrectInformations")
-        var userProfileResults: UserProfile? = nil
-        
-        // When
-        sut.fetchUserProfile { userResponse in
-            userProfileResults = userResponse
-            expectations.fulfill()
+
+        func test_WhenJSONDecode_ShouldHaveReturnCorrectInformations() {
+
+            // Given
+            let expectations: XCTestExpectation = expectation(description: "ShouldHaveCorrectInformations")
+            var userProfileResults: UserProfile? = nil
+
+            // When
+            sut.fetchUserProfile { userResponse in
+                userProfileResults = userResponse
+                expectations.fulfill()
+            }
+
+            waitForExpectations(timeout: 3.0)
+
+            // Then
+            XCTAssertEqual(userProfileResults?.name, "Teste teste")
+            XCTAssertEqual(userProfileResults?.phone, "(11) 1234-5678")
+            XCTAssertEqual(userProfileResults?.email, "teste@teste.com.br")
+            XCTAssertEqual(userProfileResults?.address, "Rua teste, 123")
+            XCTAssertEqual(userProfileResults?.account.agency, "1234")
+            XCTAssertEqual(userProfileResults?.account.account, "12345678")
         }
-        
-        waitForExpectations(timeout: 3.0)
-        
-        // Then
-        XCTAssertEqual(userProfileResults?.name, "Teste teste")
-        XCTAssertEqual(userProfileResults?.phone, "(11) 1234-5678")
-        XCTAssertEqual(userProfileResults?.email, "teste@teste.com.br")
-        XCTAssertEqual(userProfileResults?.address, "Rua teste, 123")
-        XCTAssertEqual(userProfileResults?.account.agency, "1234")
-        XCTAssertEqual(userProfileResults?.account.account, "12345678")
-    }
-    
-    func test_verifyIfJSONExist_ShouldReturnNotNil() {
-        
-        // Given
-        let expectations: XCTestExpectation = expectation(description: "Should be not nil")
-        var response: UserProfile? = nil
-        
-        // When
-        sut.fetchUserProfile { userProfileResponse in
-            response = userProfileResponse
-            expectations.fulfill()
+
+        func test_verifyIfJSONExist_ShouldReturnNotNil() {
+
+            // Given
+            let expectations: XCTestExpectation = expectation(description: "Should be not nil")
+            var response: UserProfile? = nil
+
+            // When
+            sut.fetchUserProfile { userProfileResponse in
+                response = userProfileResponse
+                expectations.fulfill()
+            }
+
+            waitForExpectations(timeout: 3.0)
+
+            // Then
+            XCTAssertNotNil(response, "The response is equal to null 🥺")
         }
-        
-        waitForExpectations(timeout: 3.0)
-        
-        // Then
-        XCTAssertNotNil(response, "The response is equal to null 🥺")
     }
 }
