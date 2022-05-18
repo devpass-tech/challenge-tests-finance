45abb308be2345a8f428e41bdaad7d5956b6e9de
matheusvmg <matheusvmg@gmail.com>
2022-05-18 20:06:55 -0300

    feat: project.pbxproj

---
+++ solutions/devsprint-attekita-3/FinanceAppTests/Service/UserProfileTests.swift
@@ -0,0 +1,21 @@
+//
+//  HomeDataTest.swift
+//  FinanceAppTests
+//
+//  Created by Matheus Góes on 17/05/22.
+//
+
+import XCTest
+@testable import FinanceApp
+
+class UserProfileTests: XCTestCase {
+
+    override func setUp() {
+        
+    }
+
+    override func tearDown() {
+        
+    }
+
+}
