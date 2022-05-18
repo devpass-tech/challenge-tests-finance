9f834bf0f3c623c1fde6d0e3d18afea58abb85ca
matheusvmg <matheusvmg@gmail.com>
2022-05-18 20:07:42 -0300

    feat: Add UserProfile formattedAccount method unit tests

--- solutions/devsprint-attekita-3/FinanceAppTests/Service/UserProfileTests.swift
+++ solutions/devsprint-attekita-3/FinanceAppTests/Service/UserProfileTests.swift
@@ -8,14 +8,30 @@
 import XCTest
 @testable import FinanceApp
 
-class UserProfileTests: XCTestCase {
+final class UserProfileTests: XCTestCase {
+    private var sut: Account!
 
     override func setUp() {
-        
+        super.setUp()
     }
 
     override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
+    
+    func testAccountStruct_formattedAccountMethod_WhenGivenAnEmptyAccountInformation_ShouldReturnAFormattedString() {
+        sut = Account(agency: "031", account: "")
+        let formattedAcountInfo = sut.formattedAccount()
         
+        XCTAssertEqual(formattedAcountInfo, "Account: ", "fails because formattedAcountInfo its not equal to 'Account: '")
+    }
+    
+    func testAccountStruct_formattedAccountMethod_WhenGivenAccountInformation_ShouldFormatCorrectly() {
+        sut = Account(agency: "031", account: "9281057193")
+        let formattedAcountInfo = sut.formattedAccount()
+        
+        XCTAssertEqual(formattedAcountInfo, "Account: \(sut.account)", "fails because formattedAcountInfo its not equal to 'Account: 9281057193'")
     }
 
 }
