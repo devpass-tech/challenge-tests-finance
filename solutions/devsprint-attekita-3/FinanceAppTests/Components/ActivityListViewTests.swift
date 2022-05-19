cba857cc65b81bfd428674b1c789a460f6d9878c
matheusvmg <matheusvmg@gmail.com>
2022-05-19 17:33:30 -0300

    feat: Add snapshot tests for ActivityListView

--- solutions/devsprint-attekita-3/FinanceAppTests/Components/ActivityListViewTests.swift
+++ solutions/devsprint-attekita-3/FinanceAppTests/Components/ActivityListViewTests.swift
@@ -6,30 +6,38 @@
 //
 
 import XCTest
+import SnapshotTesting
+@testable import FinanceApp
 
 class ActivityListViewTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    private let isRecording = false
+    private var sut: ActivityListView!
+    private var viewSize = CGSize(width: 400, height: 500)
+    private var mockVC: UIViewController!
+    
+    override func setUp() {
+        sut = ActivityListView()
+        super.setUp()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testActivityListView_WhenSnapshotAssert_ShouldBeCorrect() {
+        assertSnapshot(matching: sut, as: .image(size: viewSize), record: isRecording)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testActivityListView_WhenSnapshotAssertInMultiplesDevices_ShouldBeCorrect() {
+        mockVC = UIViewController()
+        mockVC.view = sut
+        
+        assertSnapshot(matching: mockVC, as: .image(on: .iPhone8), record: isRecording)
+        assertSnapshot(matching: mockVC, as: .image(on: .iPhoneX), record: isRecording)
+        assertSnapshot(matching: mockVC, as: .image(on: .iPhone8Plus), record: isRecording)
+        assertSnapshot(matching: mockVC, as: .image(on: .iPhoneXr), record: isRecording)
+        assertSnapshot(matching: mockVC, as: .image(on: .iPhoneXsMax), record: isRecording)
     }
 
 }
