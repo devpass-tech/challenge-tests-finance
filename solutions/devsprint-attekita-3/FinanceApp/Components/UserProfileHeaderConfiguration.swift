2308d3fd5c2908f3c64ae728cf22433f8900b045
Brun41v35 <alves.bruno945@gmail.com>
2022-05-19 16:40:26 -0300

    Creating a struct to setup the values

---
+++ solutions/devsprint-attekita-3/FinanceApp/Components/UserProfileHeaderConfiguration.swift
@@ -0,0 +1,16 @@
+//
+//  UserProfileHeaderConfiguration.swift
+//  FinanceApp
+//
+//  Created by Bruno Silva on 18/05/22.
+//
+
+import UIKit
+
+struct UserProfileHeaderConfiguration {
+    let imageView: UIImage?
+    let name: String?
+    let agency: String?
+    let account: String?
+    let bank: String?
+}
