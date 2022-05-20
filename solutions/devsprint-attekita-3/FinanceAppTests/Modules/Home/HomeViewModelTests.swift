98af2b8418c65cfa0360c02887a58bcaa8c7645b
matheusvmg <matheusvmg@gmail.com>
2022-05-19 23:15:19 -0300

    feat: Add tests for fetchData method in HomeViewModel class

--- solutions/devsprint-attekita-3/FinanceAppTests/Modules/Home/HomeViewModelTests.swift
+++ solutions/devsprint-attekita-3/FinanceAppTests/Modules/Home/HomeViewModelTests.swift
@@ -6,30 +6,40 @@
 //
 
 import XCTest
+@testable import FinanceApp
 
 class HomeViewModelTests: XCTestCase {
+    private var sut: HomeViewModel!
+    private var financeService: FinanceService!
+    private let networkClientMock = NetworkClientMock()
+    private var didFetchHomeDataCalled = false
+    private var expectations: XCTestExpectation? = nil
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override func setUp() {
+        financeService = FinanceService(networkClient: self.networkClientMock)
+        sut = HomeViewModel(financeService: self.financeService)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func tearDown() {
+        sut = nil
+        financeService = nil
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testHomeViewModel_WhenFetchDataMethodCalled_ShouldCallDidFetchHomeDataDelegateMethod() {
+        sut.delegate = self
+        expectations = self.expectation(description: "when fetchData method is executed should call didFetchHomeData delegate method")
+        
+        sut.fetchData()
+        
+        self.waitForExpectations(timeout: 3.0)
+        
+        XCTAssertTrue(self.didFetchHomeDataCalled, "fails because the fetchData method does not call the didFetchData delegate method")
     }
+}
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+extension HomeViewModelTests: HomeViewModelDelegate {
+    func didFetchHomeData(_ data: HomeData) {
+        self.didFetchHomeDataCalled = true
+        self.expectations?.fulfill()
     }
-
 }
