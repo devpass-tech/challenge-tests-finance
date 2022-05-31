8b68618d6cb9a9415cda0f2198a0297f7b8aad54
Rodrigo Borges <rborges.soares@gmail.com>
2022-05-31 14:00:11 -0300

    Base projects for Dev Sprints Caio Santos #4 and #5

---
+++ solutions/devsprint-caio-santos-5/FinanceApp/Service/Entities/TransferResult.swift
@@ -0,0 +1,13 @@
+//
+//  TransferResult.swift
+//  FinanceApp
+//
+//  Created by Rodrigo Borges on 02/03/22.
+//
+
+import Foundation
+
+struct TransferResult: Decodable {
+
+    let success: Bool
+}
