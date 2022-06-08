452e608e3b8d61ce065b57edaf26fdc663a90044
Caio Santos <caio.santos@xpi.com.br>
2022-06-08 09:04:33 -0300

    PR Adjustments.

--- solutions/devsprint-caio-santos-4/FinanceAppTests/Service/Entities/AccountTests.swift
+++ solutions/devsprint-caio-santos-4/FinanceAppTests/Service/Entities/AccountTests.swift
@@ -9,16 +9,47 @@ import XCTest
 @testable import FinanceApp
 
 class AccountTests: XCTestCase {
+    
+    // SUT: System Under Test
+    var sut: Account!
 
+    // Luiza
+    
     func test_whenInitializeAccount_thenCallFormattedAccountMethod_shouldReturnFormattedAccountInfo() {
         
         //GIVEN:
-        var sut: Account = Account(agency: "1234", account: "123456-5")
+        sut = Account(agency: "1234", account: "123456-5")
         
         //WHEN:
-        var formattedAccount = sut.formattedAccount()
+        let formattedAccount = sut.formattedAccount()
         
         //THEN:
         XCTAssertEqual(formattedAccount, "Account: 123456-5")
     }
+    
+    // Caio
+    
+    func test_formattedAccount_whenInitializeWithAccount_shouldReturnFormattedInfo() {
+        //GIVEN:
+        sut = Account(agency: "1234", account: "123456-5")
+        
+        //WHEN:
+        let formattedAccount = sut.formattedAccount()
+        
+        //THEN:
+        XCTAssertEqual(formattedAccount, "Account: 123456-5")
+    }
+    
+    
+    func test_formattedAccount_whenInitializeWithEmptyAccount_shouldReturnFormattedInfo() {
+        //GIVEN:
+        sut = Account(agency: "1234", account: "")
+        
+        //WHEN:
+        let formattedAccount = sut.formattedAccount()
+        
+        //THEN:
+        XCTAssertEqual(formattedAccount, "Account: ")
+    }
+
 }
