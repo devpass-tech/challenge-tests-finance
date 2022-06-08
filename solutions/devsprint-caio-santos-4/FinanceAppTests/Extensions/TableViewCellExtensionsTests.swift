6b1da5009007eb58de5b73879e7fd6deab287bb9
Isabella Bencardino <isabella@bencardino.com.br>
2022-06-08 13:50:12 +0100

    adicionar um teste unitário para o método classIdentifier que retorna o nome da classe

---
+++ solutions/devsprint-caio-santos-4/FinanceAppTests/Extensions/TableViewCellExtensionsTests.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCellExtensionsTests.swift
+//  FinanceAppTests
+//
+//  Created by Isabella Bencardino on 07/06/2022.
+//
+
+import XCTest
+@testable import FinanceApp
+
+class TableViewCellExtensionsTests: XCTestCase {
+
+    override func setUpWithError() throws {}
+
+    override func tearDownWithError() throws {}
+
+    func test_classIdentifier_tableViewCell_shouldReturnClassName() {
+        let activityCell = ActivityCellView.classIdentifier()
+        XCTAssertEqual(activityCell, "ActivityCellView")
+
+        let tableViewCell = UITableViewCell.classIdentifier()
+        XCTAssertEqual(tableViewCell, "UITableViewCell")
+    }
+}
