1ec683324bc5a598f94e0d7a4215d8883da7ea0d
Hyago Henrique <hyagoh@icloud.com>
2022-06-07 21:07:46 -0300

    Given, When, Then.

--- solutions/devsprint-caio-santos-5/FinanceAppTests/FinanceAppTestsStringExtension.swift
+++ solutions/devsprint-caio-santos-5/FinanceAppTests/FinanceAppTestsStringExtension.swift
@@ -11,26 +11,46 @@ import XCTest
 class FinanceAppTestsStringExtension: XCTestCase {
     
     func test_RemovingSpaces_WhenGivesFullName_ShouldReturnFullNameWithoutSpaces() {
+        // Given
         var name = "João José Silva Santana"
+        
+        // When
         name = name.removingSpaces()
+        
+        // Then
         XCTAssertEqual(name, "JoãoJoséSilvaSantana")
     }
     
     func test_RemovingSpaces_WhenGivenStringWithoutSpaces_ShouldReturnSameString() {
+        // Given
         var string = "umastringsemespaço"
+        
+        // When
         string = string.removingSpaces()
+        
+        // Then
         XCTAssertEqual(string, "umastringsemespaço")
     }
     
     func test_RemovingSpaces_WhenGivenOnlySpaces_ShouldReturnEmptyString() {
+        // Given
         var spaces = "        "
+        
+        // When
         spaces = spaces.removingSpaces()
+        
+        // Then
         XCTAssertEqual(spaces, "")
     }
     
     func test_RemovingSpaces_WhenGivenEmptyString_ShouldReturnEmptyString() {
+        // Given
         var empty = ""
+        
+        // When
         empty = empty.removingSpaces()
+        
+        // Then
         XCTAssertEqual(empty, "")
     }
 
