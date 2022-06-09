8819f1707f2ae77af3a758ca91cf88bd8ac3629f
rodrigofran <rodrigofrancms@gmail.com>
2022-06-09 19:47:57 -0300

    added Activity tests

---
+++ solutions/devsprint-caio-santos-5/FinanceAppTests/Service/Entities/ActivityTests.swift
@@ -0,0 +1,58 @@
+//
+//  ActivityTests.swift
+//  FinanceAppTests
+//
+//  Created by Rodrigo Francischett Occhiuto on 08/06/22.
+//
+
+import XCTest
+@testable import FinanceApp
+
+class ActivityTests: XCTestCase {
+    private var sut: Activity?
+    
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        sut = nil
+    }
+
+    func test_formattedInfo_WhenGivenActivityInformation_ShouldFormatCorrectly() throws {
+        // GIVEN
+        sut = Activity(name: "Farmácia", price: 50.0, time: "20")
+        let sut = try XCTUnwrap(sut, "Erro ao configurar SUT")
+        
+        // WHEN
+        let formatterInfo = sut.formattedInfo()
+
+        // THEN
+        XCTAssertEqual(formatterInfo, sut.formattedInfo())
+    }
+    
+    func test_formattedInfo_WhenGivenActivityInformation_ShouldReturnNotNill() throws {
+        // GIVEN
+        sut = Activity(name: "Farmácia",price: 50.0, time: "20")
+        let sut = try XCTUnwrap(sut, "Erro ao configurar SUT")
+        
+        // THEN
+        XCTAssertNotNil(sut.formattedInfo())
+    }
+    
+    func test_formattedInfo_WhenGivenEmptyActivityInformation_ShouldReturnEmpty() throws {
+        // GIVEN
+        sut = Activity(name: "", price: 0.0, time: "")
+        let sut = try XCTUnwrap(sut, "Erro ao configurar SUT")
+        
+        // WHEN
+        let formatterInfo = sut.formattedInfo()
+
+        // THEN
+        XCTAssertEqual(formatterInfo, "$0.0 • ")
+    }
+    
+    
+    
+    
+}
