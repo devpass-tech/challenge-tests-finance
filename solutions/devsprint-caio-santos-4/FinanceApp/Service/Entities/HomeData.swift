5c21b2aa56d8c4db5418d409932205009ea7d531
Caio Santos <caio.santos@xpi.com.br>
2022-06-10 09:14:54 -0300

    fetchHomeData tests.

--- solutions/devsprint-caio-santos-4/FinanceApp/Service/Entities/HomeData.swift
+++ solutions/devsprint-caio-santos-4/FinanceApp/Service/Entities/HomeData.swift
@@ -7,14 +7,14 @@
 
 import Foundation
 
-struct HomeData: Decodable {
+struct HomeData: Decodable, Equatable {
     let balance: Float
     let savings: Float
     let spending: Float
     let activity: [Activity]
 }
 
-struct Activity: Decodable {
+struct Activity: Decodable, Equatable {
     let name: String
     let price: Float
     let time: String
