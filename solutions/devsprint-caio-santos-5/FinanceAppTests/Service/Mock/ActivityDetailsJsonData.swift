c8d3b2d821d3967d331ef4367cc3ac5d51bb2355
Natália <natisapucaia@gmail.com>
2022-06-13 19:41:07 -0300

    implementando teste em fetchActivityDetails

---
+++ solutions/devsprint-caio-santos-5/FinanceAppTests/Service/Mock/ActivityDetailsJsonData.swift
@@ -0,0 +1,17 @@
+//
+//  ActivityDetails.swift
+//  FinanceAppTests
+//
+//  Created by Natália Sapucaia on 13/06/22.
+//
+
+import Foundation
+
+let ActivityDetailsJsonData = """
+{
+    "name": "Mall",
+    "category": "Shopping",
+    "price": 100.0,
+    "time": "8:57 AM"
+}
+""".data(using: .utf8)
