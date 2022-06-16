446d312ec2934c64e437140f837090f98fbdfb34
Virgilius <virgilius_santos@yahoo.com.br>
2022-06-16 19:36:07 -0300

    fix snapshots

--- solutions/devsprint-caio-santos-5/FinanceAppTests/SnapshotTests/HomeHeaderViewSnapshotTests.swift
+++ solutions/devsprint-caio-santos-5/FinanceAppTests/SnapshotTests/HomeHeaderViewSnapshotTests.swift
@@ -1,15 +1,8 @@
-//
-//  HomeHeaderViewSnapshotTests.swift
-//  FinanceAppTests
-//
-//  Created by Caio Santos on 14/06/22.
-//
-
 import XCTest
 import SnapshotTesting
 @testable import FinanceApp
 
-class HomeHeaderViewSnapshotTests: XCTestCase {
+final class HomeHeaderViewSnapshotTests: XCTestCase {
     let componentSize = CGSize(width: 414, height: 149)
     
     override func setUp() {
