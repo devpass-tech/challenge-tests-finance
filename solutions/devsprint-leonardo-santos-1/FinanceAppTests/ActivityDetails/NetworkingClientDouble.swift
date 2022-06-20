8d9e313c826b7fe299b549cac15099c71b69360f
Hoff Silva <hoff.henry@gmail.com>
2022-06-20 19:58:01 -0400

    Create NetworkingClientDouble

---
+++ solutions/devsprint-leonardo-santos-1/FinanceAppTests/ActivityDetails/NetworkingClientDouble.swift
@@ -0,0 +1,25 @@
+//
+//  NetworkingClientDouble.swift
+//  FinanceAppTests
+//
+//  Created by Hoff Silva on 2022-06-20.
+//
+
+import Foundation
+
+@testable import FinanceApp
+
+struct NetworkingClientDouble: NetworkClientProtocol {
+    func performRequest(with url: URL, completion: @escaping (Data?) -> ()) {
+        let data = """
+        {
+            "name": "Mall",
+            "category": "Shopping",
+            "price": 100.0,
+            "time": "8:57 AM"
+        }
+        """.data(using: .utf8)
+
+        completion(data)
+    }
+}
