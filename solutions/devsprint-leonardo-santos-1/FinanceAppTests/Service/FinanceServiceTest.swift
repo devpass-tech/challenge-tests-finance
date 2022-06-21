53a383f097e56e714629c60bdcf2d272b7f20f85
Leonardo Cunha <leonardo.cunha@usemobile.xyz>
2022-06-21 10:49:53 -0300

    🏗: Created test class file

---
+++ solutions/devsprint-leonardo-santos-1/FinanceAppTests/Service/FinanceServiceTest.swift
@@ -0,0 +1,14 @@
+//
+//  FinanceServiceTest.swift
+//  FinanceAppTests
+//
+//  Created by Leonardo Cunha on 21/06/22.
+//
+
+import XCTest
+@testable import FinanceApp
+
+class FinanceServiceTest: XCTestCase {
+    
+    
+}
