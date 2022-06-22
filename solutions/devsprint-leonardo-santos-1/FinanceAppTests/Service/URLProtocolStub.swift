78bb437e4c182df14293717e2517f1cc0c59ce7d
Hoff Silva <hoff.henry@gmail.com>
2022-06-21 23:26:06 -0400

    Create URLProtocolStub to intercept the requests.

--- solutions/devsprint-leonardo-santos-1/FinanceAppTests/Service/URLProtocolStub.swift
+++ solutions/devsprint-leonardo-santos-1/FinanceAppTests/Service/URLProtocolStub.swift
@@ -8,20 +8,6 @@
 import Foundation
 
 class URLProtocolStub: URLProtocol {
-    static var emit: ((URLRequest) -> Void)?
-    static var data: Data?
-    static var response: HTTPURLResponse?
-    static var error: Error?
-    
-    static func observeRequest(completion: @escaping (URLRequest) -> Void) {
-        URLProtocolStub.emit = completion
-    }
-    
-    static func simulate(data: Data?, response: HTTPURLResponse?, error: Error?) {
-        URLProtocolStub.data = data
-        URLProtocolStub.response = response
-        URLProtocolStub.error = error
-    }
     
     override class func canInit(with request: URLRequest) -> Bool {
         true
@@ -32,20 +18,6 @@ class URLProtocolStub: URLProtocol {
     }
     
     override func startLoading() {
-        URLProtocolStub.emit?(request)
-
-        if let data = URLProtocolStub.data {
-            client?.urlProtocol(self, didLoad: data)
-        }
-
-        if let response = URLProtocolStub.response {
-            client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
-        }
-
-        if let error = URLProtocolStub.error {
-            client?.urlProtocol(self, didFailWithError: error)
-        }
-
         client?.urlProtocolDidFinishLoading(self)
     }
     
