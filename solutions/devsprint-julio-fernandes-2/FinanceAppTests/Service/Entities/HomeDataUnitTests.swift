12bc2fe75d3e13b76980c22656f9f56a842ebe63
Gabriela Sillis <gabydias47@gmail.com>
2022-06-22 18:34:48 -0300

    refactor: renomeia variavel

--- solutions/devsprint-julio-fernandes-2/FinanceAppTests/Service/Entities/HomeDataUnitTests.swift
+++ solutions/devsprint-julio-fernandes-2/FinanceAppTests/Service/Entities/HomeDataUnitTests.swift
@@ -9,10 +9,10 @@ import XCTest
 @testable import FinanceApp
 
 final class HomeDataUnitTests: XCTestCase {
-    let activity = Activity(name: "any_name", price: 80, time: "18000")
+    private let sut = Activity(name: "any_name", price: 80, time: "18000")
     
     func test_formattedInfo_shouldReturn_formattedString() {
         let expedtedString = "$80.0 • 18000"
-        XCTAssertEqual(activity.formattedInfo(), expedtedString)
+        XCTAssertEqual(sut.formattedInfo(), expedtedString)
     }
 }
