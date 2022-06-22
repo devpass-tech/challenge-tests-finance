90bb98b314139623928e4582fafd8e9a74815293
Marcio Aun Migueis <marcioa1@gmail.com>
2022-06-22 18:07:42 -0300

    Update solutions/devsprint-leonardo-santos-1/FinanceAppTests/AccountTest.swift
    Co-authored-by: Leonardo <42387881+leocoout@users.noreply.github.com>

--- solutions/devsprint-leonardo-santos-1/FinanceAppTests/AccountTest.swift
+++ solutions/devsprint-leonardo-santos-1/FinanceAppTests/AccountTest.swift
@@ -9,9 +9,9 @@ import XCTest
 @testable import FinanceApp
 
 class AccountTest: XCTestCase {
+    private let sut = Account(agency: "123", account: "4567")
 
     func test_formattedAgency_shouldFormatAgency() {
-        let account = Account(agency: "123", account: "4567")
         XCTAssertEqual(account.formattedAgency(), "Agency: 123")
     }
 }
