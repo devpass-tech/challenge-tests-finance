1a5d7abe738b5f33fe3fc86926442bb4a9abeef7
Hoff Silva <hoff.henry@gmail.com>
2022-06-24 08:02:28 -0400

    Added NetworkClientTests

---
+++ solutions/devsprint-leonardo-santos-1/FinanceAppTests/Helpers/Fixtures.swift
@@ -0,0 +1,47 @@
+//
+//  Fixtures.swift
+//  FinanceAppTests
+//
+//  Created by Hoff Silva on 2022-06-24.
+//
+
+import Foundation
+
+extension URL {
+    static func fixture() -> Self {
+        URL(string: "https://")!
+    }
+}
+
+extension Error {
+    static func fixture() -> Self {
+        NSError(domain: "", code: 0) as! Self
+    }
+}
+
+extension Data {
+    static func fixture() -> Self {
+        """
+        {
+            "name": "Mall",
+            "category": "Shopping",
+            "price": 100.0,
+            "time": "8:57 AM"
+        }
+        """.data(using: .utf8)!
+    }
+}
+
+extension HTTPURLResponse {
+    static func succesFixture() -> Self {
+        HTTPURLResponse(url: URL.fixture(), statusCode: Int.random(in: 200...299), httpVersion: nil, headerFields: nil) as! Self
+    }
+    
+    static func clientErrorFixture() -> Self {
+        HTTPURLResponse(url: URL.fixture(), statusCode: Int.random(in: 400...451), httpVersion: nil, headerFields: nil) as! Self
+    }
+    
+    static func serverErrorFixture() -> Self {
+        HTTPURLResponse(url: URL.fixture(), statusCode: Int.random(in: 500...511), httpVersion: nil, headerFields: nil) as! Self
+    }
+}
