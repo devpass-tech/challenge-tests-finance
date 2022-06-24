3ea924fc726e405b35c2fdbe1b006027143f3698
Hoff Silva <hoff.henry@gmail.com>
2022-06-24 08:12:23 -0400

    Added tests covering http status code.

--- solutions/devsprint-leonardo-santos-1/FinanceAppTests/Service/NetworkClientTests.swift
+++ solutions/devsprint-leonardo-santos-1/FinanceAppTests/Service/NetworkClientTests.swift
@@ -12,79 +12,79 @@ import XCTest
 final class NetworkClientTests: XCTestCase {
     
     private let sut: NetworkClient = { NetworkClient() }()
-    
-    func test_performRequest_whenCompletesWithError_shouldReturnNil() {
-        URLProtocol.registerClass(URLProtocolStub.self)
-        URLProtocolStub.configureRequest(data: nil, response: nil, and: NSError.fixture())
-        let expec = expectation(description: "")
-        sut.performRequest(with: URL.fixture() , completion: { data in
-            XCTAssertNil(data)
-            expec.fulfill()
-        })
-        wait(for: [expec], timeout: 1)
-    }
-    
-    func test_performRequest_whenCompletesWithResponse_shouldReturnNotNil() {
-        URLProtocol.registerClass(URLProtocolStub.self)
-        URLProtocolStub.configureRequest(data: nil, response: HTTPURLResponse.succesFixture(), and: nil)
-        let expec = expectation(description: "")
-        sut.performRequest(with: URL.fixture() , completion: { data in
-            XCTAssertNotNil(data)
-            expec.fulfill()
-        })
-        wait(for: [expec], timeout: 1)
-    }
-    
-    func test_performRequest_whenCompletesOnlyWithData_shouldReturnNil() {
-        URLProtocol.registerClass(URLProtocolStub.self)
-        URLProtocolStub.configureRequest(data: Data.fixture(), response: nil, and: nil)
-        let expec = expectation(description: "")
-        sut.performRequest(with: URL.fixture() , completion: { data in
-            XCTAssertNil(data)
-            expec.fulfill()
-        })
-        wait(for: [expec], timeout: 1)
-    }
-    
-    func test_performRequest_whenCompletesWithHTTPStatusCode2XX_shouldReturnNotNil() {
-        URLProtocol.registerClass(URLProtocolStub.self)
-        URLProtocolStub.configureRequest(data: Data.fixture(), response: HTTPURLResponse.succesFixture(), and: nil)
-        let expec = expectation(description: "")
-        sut.performRequest(with: URL.fixture() , completion: { data in
-            XCTAssertNotNil(data)
-            expec.fulfill()
-        })
-        wait(for: [expec], timeout: 1)
-    }
-    
-    func test_performRequest_whenCompletesWithHTTPStatusCode4XX_shouldReturnNil() {
-        URLProtocol.registerClass(URLProtocolStub.self)
-        URLProtocolStub.configureRequest(data: nil, response: HTTPURLResponse.clientErrorFixture(), and: nil)
-        let expec = expectation(description: "")
-        sut.performRequest(with: URL.fixture() , completion: { data in
-            XCTAssertNil(data)
-            expec.fulfill()
-        })
-        wait(for: [expec], timeout: 1)
-    }
-    
-    func test_performRequest_whenCompletesWithHTTPStatusCode5XX_shouldReturnNil() {
-        URLProtocol.registerClass(URLProtocolStub.self)
-        URLProtocolStub.configureRequest(data: nil, response: HTTPURLResponse.serverErrorFixture(), and: nil)
-        let expec = expectation(description: "")
-        sut.performRequest(with: URL.fixture() , completion: { data in
-            XCTAssertNil(data)
-            expec.fulfill()
-        })
-        wait(for: [expec], timeout: 1)
-    }
-    
-    func test_performRequest_whenURLIsPassed_requestShouldReceiveTheSameURL() {
-        URLProtocol.registerClass(URLProtocolStub.self)
-        URLProtocolStub.subscribe { request in
-            XCTAssertEqual(URL.fixture(), request.url)
-        }
-        sut.performRequest(with: URL.fixture() , completion: { _ in })
-    }
+       
+       func test_performRequest_whenCompletesWithError_shouldReturnNil() {
+           URLProtocol.registerClass(URLProtocolStub.self)
+           URLProtocolStub.configureRequest(data: nil, response: nil, and: NSError.fixture())
+           let expec = expectation(description: "")
+           sut.performRequest(with: URL.fixture() , completion: { data in
+               XCTAssertNil(data)
+               expec.fulfill()
+           })
+           wait(for: [expec], timeout: 1)
+       }
+       
+       func test_performRequest_whenCompletesWithResponse_shouldReturnNotNil() {
+           URLProtocol.registerClass(URLProtocolStub.self)
+           URLProtocolStub.configureRequest(data: nil, response: HTTPURLResponse.succesFixture(), and: nil)
+           let expec = expectation(description: "")
+           sut.performRequest(with: URL.fixture() , completion: { data in
+               XCTAssertNotNil(data)
+               expec.fulfill()
+           })
+           wait(for: [expec], timeout: 1)
+       }
+       
+       func test_performRequest_whenCompletesOnlyWithData_shouldReturnNil() {
+           URLProtocol.registerClass(URLProtocolStub.self)
+           URLProtocolStub.configureRequest(data: Data.fixture(), response: nil, and: nil)
+           let expec = expectation(description: "")
+           sut.performRequest(with: URL.fixture() , completion: { data in
+               XCTAssertNil(data)
+               expec.fulfill()
+           })
+           wait(for: [expec], timeout: 1)
+       }
+       
+       func test_performRequest_whenCompletesWithHTTPStatusCode2XX_shouldReturnNotNil() {
+           URLProtocol.registerClass(URLProtocolStub.self)
+           URLProtocolStub.configureRequest(data: Data.fixture(), response: HTTPURLResponse.succesFixture(), and: nil)
+           let expec = expectation(description: "")
+           sut.performRequest(with: URL.fixture() , completion: { data in
+               XCTAssertNotNil(data)
+               expec.fulfill()
+           })
+           wait(for: [expec], timeout: 1)
+       }
+       
+       func test_performRequest_whenCompletesWithHTTPStatusCode4XX_shouldReturnNil() {
+           URLProtocol.registerClass(URLProtocolStub.self)
+           URLProtocolStub.configureRequest(data: nil, response: HTTPURLResponse.clientErrorFixture(), and: nil)
+           let expec = expectation(description: "")
+           sut.performRequest(with: URL.fixture() , completion: { data in
+               XCTAssertNil(data)
+               expec.fulfill()
+           })
+           wait(for: [expec], timeout: 1)
+       }
+       
+       func test_performRequest_whenCompletesWithHTTPStatusCode5XX_shouldReturnNil() {
+           URLProtocol.registerClass(URLProtocolStub.self)
+           URLProtocolStub.configureRequest(data: nil, response: HTTPURLResponse.serverErrorFixture(), and: nil)
+           let expec = expectation(description: "")
+           sut.performRequest(with: URL.fixture() , completion: { data in
+               XCTAssertNil(data)
+               expec.fulfill()
+           })
+           wait(for: [expec], timeout: 1)
+       }
+       
+       func test_performRequest_whenURLIsPassed_requestShouldReceiveTheSameURL() {
+           URLProtocol.registerClass(URLProtocolStub.self)
+           URLProtocolStub.subscribe { request in
+               XCTAssertEqual(URL.fixture(), request.url)
+           }
+           sut.performRequest(with: URL.fixture() , completion: { _ in })
+       }
     
 }
