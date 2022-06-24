5d922f212792311fc70fb2c0602859dad92cc3d1
Hoff Silva <hoff.henry@gmail.com>
2022-06-24 08:10:58 -0400

    Code improvements on network client perform request.

--- solutions/devsprint-leonardo-santos-1/FinanceApp/Service/NetworkClient.swift
+++ solutions/devsprint-leonardo-santos-1/FinanceApp/Service/NetworkClient.swift
@@ -17,21 +17,33 @@ final class NetworkClient: NetworkClientProtocol {
     func performRequest(with url: URL, completion: @escaping (Data?) -> ()) {
 
         let request = URLRequest(url: url)
-
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-
-            if let _ = error {
-                completion(nil)
-                return
-            }
-
-            guard let data = data else {
-                completion(nil)
-                return
-            }
-
-            completion(data)
-        }
-        task.resume()
+                
+                let task = URLSession.shared.dataTask(with: request) { data, response, error in
+                    
+                    if let _ = error {
+                        completion(nil)
+                        return
+                    }
+                    
+                    guard let response = response as? HTTPURLResponse else {
+                        completion(nil)
+                        return
+                    }
+                    
+                    switch response.statusCode {
+                    case 200...299:
+                        guard let data = data else {
+                            completion(nil)
+                            return
+                        }
+                        completion(data)
+                    case 400...499, 500...599:
+                        completion(nil)
+                    default:
+                        completion(nil)
+                    }
+                    
+                }
+                task.resume()
     }
 }
