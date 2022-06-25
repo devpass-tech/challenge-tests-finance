4390a6915a6031b6633a18f953b493b5b8953863
Junior Margalho <nelson.junior@usemobile.xyz>
2022-06-25 07:50:32 -0300

    ⭐️: alter FinanceServiceTests

--- solutions/devsprint-leonardo-santos-1/FinanceAppTests/Service/FinanceServiceTests.swift
+++ solutions/devsprint-leonardo-santos-1/FinanceAppTests/Service/FinanceServiceTests.swift
@@ -13,15 +13,17 @@ final class FinanceServiceTests: XCTestCase {
     private let networkSpy = NetworkClientSpy()
     private lazy var sut = FinanceService(networkClient: networkSpy)
 
-    func testFetchHomeDataShouldEqualsMockData() {
-        let homeDataFake = HomeData.fixture(balance: 15459.27,
-                                            savings: 1000.0,
-                                            spending: 500.0,
-                                            activity: [Activity(name: "Mall",
-                                                                price: 100.0,
-                                                                time: "8:57 AM")])
-        sut.fetchHomeData { (homeData) in
-            XCTAssertEqual(homeData, homeDataFake)
+    func testFetchHomeDataGivenURLShoulCallNetworkClientWithCorrectURL() {
+        sut.fetchHomeData { [networkSpy] _ in
+            XCTAssertTrue(networkSpy.performRequestCalled)
+            XCTAssertEqual(networkSpy.performRequestCallCount, 1)
+            XCTAssertEqual(networkSpy.performRequestUrlPassed?.absoluteString, Constants.fetchHomeDataURLString)
         }
+    }
+}
+
+private extension FinanceServiceTests {
+    enum Constants {
+        static let fetchHomeDataURLString = "https://raw.githubusercontent.com/devpass-tech/challenge-finance-app/main/api/home_endpoint.json"
     }
 }
