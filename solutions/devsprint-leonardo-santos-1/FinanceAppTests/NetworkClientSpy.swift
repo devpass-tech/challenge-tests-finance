c0ae289bb60320e2aa186043d7646f52aeda74eb
Hoff Silva <hoff.henry@gmail.com>
2022-06-27 17:47:53 -0400

    Added dataToBeReturned.

--- solutions/devsprint-leonardo-santos-1/FinanceAppTests/NetworkClientSpy.swift
+++ solutions/devsprint-leonardo-santos-1/FinanceAppTests/NetworkClientSpy.swift
@@ -13,10 +13,16 @@ final class NetworClientSpy: NetworkClientProtocol {
     
     private(set) var passedURL: URL?
     private(set) var performRequestCallCount: Int = 0
+    let dataToBeReturned: Data?
+    
+    init(dataToBeReturned: Data?) {
+        self.dataToBeReturned = dataToBeReturned
+    }
     
     func performRequest(with url: URL, completion: @escaping (Data?) -> ()) {
         passedURL = url
         performRequestCallCount += 1
+        completion(dataToBeReturned)
     }
     
 }
