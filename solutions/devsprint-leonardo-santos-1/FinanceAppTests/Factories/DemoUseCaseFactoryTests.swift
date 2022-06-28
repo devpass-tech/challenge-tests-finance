786817bf0d364b069ac50c30bc567a644b5d6ee8
Hoff Silva <hoff.henry@gmail.com>
2022-06-28 18:51:04 -0400

    Added DemoUseCaseFactoryTests file

---
+++ solutions/devsprint-leonardo-santos-1/FinanceAppTests/Factories/DemoUseCaseFactoryTests.swift
@@ -0,0 +1,20 @@
+//
+//  DemoUseCaseFactoryTests.swift
+//  FinanceAppTests
+//
+//  Created by Hoff Silva on 2022-06-28.
+//
+
+import XCTest
+
+@testable import FinanceApp
+
+final class DemoUseCaseFactoryTests: XCTestCase {
+    
+    private let sut = DemoUseCaseFactory()
+    
+    func test_() {
+        
+    }
+    
+}
