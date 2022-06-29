86a6348a815a9104ef7371d8565c3e94fe2b1739
Hoff Silva <hoff.henry@gmail.com>
2022-06-29 06:44:16 -0400

    Added URL extension.

---
+++ solutions/devsprint-leonardo-santos-1/FinanceAppTests/Helpers/URL+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  URL+Extension.swift
+//  FinanceAppTests
+//
+//  Created by Hoff Silva on 2022-06-29.
+//
+
+import Foundation
+
+extension URL {
+    init(with string: String = "https://") {
+       self.init(string: string)!
+    }
+}
