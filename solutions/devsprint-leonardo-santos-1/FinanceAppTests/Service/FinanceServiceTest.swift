a2b6264c53253d2b24796608a14eda014793f538
leonardosantos-ifood <leonardo.santos@ifood.com.br>
2022-07-01 17:09:43 -0300

    Fix merged MR causing project not open

--- solutions/devsprint-leonardo-santos-1/FinanceAppTests/Service/FinanceServiceTest.swift
+++ solutions/devsprint-leonardo-santos-1/FinanceAppTests/Service/FinanceServiceTest.swift
@@ -49,23 +49,93 @@ final class FinanceServiceTests: XCTestCase {
         }
     }
     
+    // MARK: - fetchHomeData
+    
+    func testFetchHomeDataGivenURLShoulCallNetworkClientWithCorrectURL() {
+        sut.fetchHomeData { [networkSpy] _ in
+            XCTAssertTrue(networkSpy.performRequestCalled)
+            XCTAssertEqual(networkSpy.performRequestCount, 1)
+            XCTAssertEqual(networkSpy.performRequestPassed, self.fetchHomeDataURLString)
+        }
+    }
+    
+    func testFetchHomeDataWhenDataIsNilshouldReturnNil() {
+        networkSpy.dataToBeReturned = nil
+        
+        sut.fetchHomeData { response in
+            XCTAssertNil(response)
+        }
+    }
+    
+    func testFetchHomeDataWhenDataIsNotNilWhenDecodeSucceedShouldReturnCorrectHomeData() {
+        networkSpy.dataToBeReturned = fetchHomeData
+        
+        sut.fetchHomeData { response in
+            XCTAssertNil(response)
+        }
+    }
+    
+    func testFetchHomeDataWhenDataIsNotNilWhenDecodeErrorShouldReturnNilHomeData() {
+        networkSpy.dataToBeReturned = dataWithError
+        
+        sut.fetchHomeData { response in
+            XCTAssertNil(response)
+        }
+    }
+    
     // MARK: Private properties
-    private let contactListData =  {
-"""
-[
-{
-    "name": "Ronald Robertson",
-    "phone": "+55 (11) 99999-9999"
-},
-{
-    "name": "Johnny Watson",
-    "phone": "+55 (11) 99999-9999"
-}
-]
-""".data(using: .utf8)
-    }()
     
     private let dataWithError = "".data(using: .utf8)
-    
     private let fetchContactListUrlString = "https://raw.githubusercontent.com/devpass-tech/challenge-finance-app/main/api/contact_list_endpoint.json"
+    private let fetchHomeDataURLString = "https://raw.githubusercontent.com/devpass-tech/challenge-finance-app/main/api/home_endpoint.json"
+    
+    private let contactListData =
+        """
+        [
+        {
+        "name": "Ronald Robertson",
+        "phone": "+55 (11) 99999-9999"
+        },
+        {
+        "name": "Johnny Watson",
+        "phone": "+55 (11) 99999-9999"
+        }
+        ]
+        """.data(using: .utf8)
+    
+    private let fetchHomeData =
+        """
+        {
+          balance: 15459.27,
+          savings: 1000.0,
+          spending: 500.0,
+          activity: [
+            {
+              name: "Mall",
+              price: 100.0,
+              time: "8:57 AM",
+            },
+            {
+              name": "Food Court",
+              price": 100.0,
+              time": "8:57 AM",
+            },
+            {
+              name: "Oceanic Airlines",
+              price: 100.0,
+              time: "8:57 AM",
+            },
+            {
+              name: "Gym Membership",
+              price: 100.0,
+              time: "8:57 AM",
+            },
+            {
+              name: "Private Transport",
+              price: 100.0,
+              time: "8:57 AM",
+            },
+          ],
+        }
+        """.data(using: .utf8)
 }
