a760e71a712b67c108203ab2bde0f30a24948646
Leonardo Cunha <leonardo.cunha@usemobile.xyz>
2022-07-05 09:39:52 -0300

    ✨finished tests for AnySdk

--- solutions/devsprint-leonardo-santos-1/FinanceAppTests/ThirdyPartySDK/ThirdyPartySDKTests.swift
+++ solutions/devsprint-leonardo-santos-1/FinanceAppTests/ThirdyPartySDK/ThirdyPartySDKTests.swift
@@ -10,15 +10,37 @@ import XCTest
 
 final class ThirdyPartySDKTests: XCTestCase {
     private let sdkSpy = AnySdkSpy()
-    private let isEnabled = false
+    private var isEnabled = false
     private lazy var sut = AnySDKFirstImpl(isEnabled: isEnabled, sdk: sdkSpy)
     
     override func setUp() {
         type(of: sdkSpy).resetCount()
     }
     
-    func test_whenIsEnabled_shouldInitSDK() {
-        
+    func test_initSdk_whenEnabled_shouldCallInitSDK() {
+        isEnabled = true
+        sut.initSDK()
+        XCTAssertEqual((type(of: sdkSpy).initSdkCallCount), 1)
     }
     
+    func test_initSdk_whenIsNotEnabled_shouldNotCallInitSDK() {
+        isEnabled = false
+        sut.initSDK()
+        XCTAssertEqual((type(of: sdkSpy).initSdkCallCount), 0)
+    }
+    
+    func test_trackEvent_whenIsEnabled_shouldTrackEventPassedOnParameter() {
+        isEnabled = true
+        type(of: sdkSpy).trackEvent("shouldTrackThis")
+        
+        XCTAssertEqual((type(of: sdkSpy).trackEventCallCount), 1)
+        XCTAssertEqual((type(of: sdkSpy).eventNamePassed), "shouldTrackThis")
+    }
+    
+    func test_trackEvent_whenIsNotEnabled_shouldNotCallTrackEvents() {
+        isEnabled = false
+        type(of: sdkSpy).trackEvent("shouldNotTrackThis")
+        
+        XCTAssertEqual((type(of: sdkSpy).trackEventCallCount), 0)
+    }
 }
