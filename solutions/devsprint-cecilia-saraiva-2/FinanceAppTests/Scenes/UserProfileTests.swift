8a1afa9c12932150af565bd507090a2681288484
Henrique Cora <henrique@cora.com.br>
2022-08-15 18:44:47 -0300

    Teste unitário para o metodo formattedAccount

---
+++ solutions/devsprint-cecilia-saraiva-2/FinanceAppTests/Scenes/UserProfileTests.swift
@@ -0,0 +1,22 @@
+//
+//  UserProfileTests.swift
+//  FinanceAppTests
+//
+//  Created by Henrique Augusto on 15/08/22.
+//
+
+import XCTest
+@testable import FinanceApp
+
+final class UserProfileTests: XCTestCase {
+    func test_ShouldReturnAccountFormatted_WhenCallMethod() {
+        let firstAccount = Account(agency: "11111", account: "11111")
+        let secondAccount = Account(agency: "22222", account: "22222")
+        
+        let firstFormattedAccount = firstAccount.formattedAccount()
+        let secondFormattedAccount = secondAccount.formattedAccount()
+        
+        XCTAssertEqual(firstFormattedAccount, "Account: 11111")
+        XCTAssertEqual(secondFormattedAccount, "Account: 22222")
+    }
+}
