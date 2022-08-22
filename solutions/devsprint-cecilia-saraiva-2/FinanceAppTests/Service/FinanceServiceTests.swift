b8b459ab07598dfbf713eaadfc30bc71bcee5b18
vitor-rc1 <vitor_rc@outlook.com>
2022-08-22 18:50:51 -0300

    Applied suggestion from code review

--- solutions/devsprint-cecilia-saraiva-2/FinanceAppTests/Service/FinanceServiceTests.swift
+++ solutions/devsprint-cecilia-saraiva-2/FinanceAppTests/Service/FinanceServiceTests.swift
@@ -14,27 +14,33 @@ final class FinanceServiceTests: XCTestCase {
         let (sut, networkClient) = makeSUT()
         let stringURL = "https://raw.githubusercontent.com/devpass-tech/challenge-finance-app/main/api/home_endpoint.json"
         
-        sut.fetchHomeData { _ in
-            XCTAssertEqual(networkClient.calledMethods, [.performRequest(url: stringURL)])
-        }
+        sut.fetchHomeData { _ in }
+        
+        XCTAssertEqual(networkClient.calledMethods, [.performRequest(url: stringURL)])
     }
 
     func test_fetchHomeData_shouldReturnNilWhenRequestReturnNil() {
         let (sut, networkClient) = makeSUT()
+        var out: HomeData?
         
         sut.fetchHomeData { response in
-            XCTAssertTrue(networkClient.wasPerformRequestCalled)
-            XCTAssertNil(response)
+            out = response
         }
+        
+        XCTAssertTrue(networkClient.wasPerformRequestCalled)
+        XCTAssertNil(out)
     }
     
     func test_fetchHomeData_shouldReturnNilWhenRequestReturnEmptyData() {
         let (sut, networkClient) = makeSUT(data: Data())
+        var out: HomeData?
         
         sut.fetchHomeData { response in
-            XCTAssertTrue(networkClient.wasPerformRequestCalled)
-            XCTAssertNil(response)
+            out = response
         }
+        
+        XCTAssertTrue(networkClient.wasPerformRequestCalled)
+        XCTAssertNil(out)
     }
     
     func test_fetchHomeData_shouldReturnHomeDataWhenRequestReturnCorrectData() throws {
@@ -42,12 +48,15 @@ final class FinanceServiceTests: XCTestCase {
         let homeData = HomeData(balance: 100.0, savings: 200.0, spending: 150, activity: [activity])
         let encodedHomeData = try XCTUnwrap(JSONEncoder().encode(homeData))
         let (sut, networkClient) = makeSUT(data: Data(encodedHomeData))
+        var out: HomeData?
         
         sut.fetchHomeData { response in
-            XCTAssertTrue(networkClient.wasPerformRequestCalled)
-            XCTAssertEqual(response?.balance, 100.0)
-            XCTAssertEqual(response?.activity.first?.name, "Market")
+            out = response
         }
+        
+        XCTAssertTrue(networkClient.wasPerformRequestCalled)
+        XCTAssertEqual(out?.balance, 100.0)
+        XCTAssertEqual(out?.activity.first?.name, "Market")
     }
 }
 
