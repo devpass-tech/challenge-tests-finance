385d9f7a419ec3fab5ba50d1715219f4e36309df
Leonardo Almeida <leonardograselalmeida@gmail.com>
2022-08-24 18:19:14 -0300

    test(Activity) fix code review notes

--- solutions/devsprint-cecilia-saraiva-2/FinanceAppTests/Service/Entities/ActivityTests.swift
+++ solutions/devsprint-cecilia-saraiva-2/FinanceAppTests/Service/Entities/ActivityTests.swift
@@ -8,16 +8,11 @@
 import XCTest
 @testable import FinanceApp
 
-class ActivityTests: XCTestCase {
+final class ActivityTests: XCTestCase {
 
-    func testActivityFormattedInfoSuccess() throws {
+    func testActivityFormattedInfoSuccess() {
         let activity: Activity = .init(name: "Teste", price: 20.9, time: "10:00")
         XCTAssertEqual(activity.formattedInfo(), "$20.9 • 10:00")
-    }
-    
-    func testActivityFormattedInfoFailure() throws {
-        let activity: Activity = .init(name: "Teste", price: 20.9, time: "10:00")
-        XCTAssertNotEqual(activity.formattedInfo(), "$20.9 • 10:01")
     }
     
 }
