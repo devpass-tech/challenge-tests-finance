b8cd0304d4e4bfee6b4b9b2239fa8bc2decdd32f
Raline <ralinesilva1@gmail.com>
2022-08-24 16:51:10 -0300

    create test fetch contact list

--- solutions/devsprint-cecilia-saraiva-2/FinanceAppTests/Service/FinanceServiceTests.swift
+++ solutions/devsprint-cecilia-saraiva-2/FinanceAppTests/Service/FinanceServiceTests.swift
@@ -6,30 +6,58 @@
 //
 
 import XCTest
+@testable import FinanceApp
 
-class FinanceServiceTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+final class FinanceServiceTests: XCTestCase {
+    
+    private let networkClient = NetworkClientMock()
+    private lazy var mock = FinanceService(networkClient: networkClient)
+    var contact: [Contact]?
+    private let dataWithError = "".data(using: .utf8)
+    var passedURL: URL?
+       
+    override func setUp() {
+        passedURL = URL(string: "https://raw.githubusercontent.com/devpass-tech/challenge-finance-app/main/api/contact_list_endpoint.json")!
+    }
+    
+    override func tearDown() {
+        self.passedURL = nil
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testFetchContactListSucessWhenJSONIsValid() {
+        networkClient.data = networkClient.contactListData
+        let mockContactList = [Contact.list(name: "Ana",
+                                            phone: "+55 (81) 99999-9999"),
+                               Contact.list(name: "Maria",
+                                            phone: "+55 (81) 11111-1111")]
+        mock.fetchContactList { contactList in
+            self.contact = contactList
         }
+        
+        XCTAssertEqual(contact, mockContactList)
+        XCTAssertEqual(networkClient.perfomRequestCallCount, 1)
+        XCTAssertEqual(networkClient.url, passedURL)
+        
     }
-
+    
+    func testFetchContactListWhenIncomingDataWithReturnNill() {
+        networkClient.data = nil
+       
+        mock.fetchContactList { contactList in
+            self.contact = contactList
+        }
+        
+        XCTAssertNil(contact)
+    }
+    
+    func testFetchContactListWhenJSONDecodeFailsReturnNil() {
+        networkClient.data = dataWithError
+        
+        mock.fetchContactList { contactList in
+            self.contact = contactList
+        }
+        
+        XCTAssertNil(contact)
+    }
+ 
 }
