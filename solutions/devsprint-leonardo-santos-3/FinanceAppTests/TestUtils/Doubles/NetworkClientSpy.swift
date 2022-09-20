b1d80b0581c7fdee20c2528406de77150fb5486b
Pedro Gabriel <pee.torres@gmail.com>
2022-09-20 09:30:09 +0100

    Create NetworkClientSpy

--- solutions/devsprint-leonardo-santos-3/FinanceAppTests/TestUtils/Doubles/NetworkClientSpy.swift
+++ solutions/devsprint-leonardo-santos-3/FinanceAppTests/TestUtils/Doubles/NetworkClientSpy.swift
@@ -1,1 +1,15 @@
+import Foundation
 
+@testable import FinanceApp
+
+class NetworkClientSpy: NetworkClientProtocol {
+    // MARK: Properties
+    var completionData: Data?
+    var performRequestCount: Int = 0
+
+    // MARK: Methods
+    func performRequest(with url: URL, completion: @escaping (Data?) -> ()) {
+        performRequestCount += 1
+        completion(completionData)
+    }
+}
