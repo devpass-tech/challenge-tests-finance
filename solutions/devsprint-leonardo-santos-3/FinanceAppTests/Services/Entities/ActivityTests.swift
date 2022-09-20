e43a32abf105b4a415ebb1b3f46134a519c5410b
Luiza Avelino <avelino.luiza98@gmail.com>
2022-09-20 12:52:54 -0700

    ✨ formattedInfo tests (#164)
    * add: formattedInfo tests
    
    * remove extra spaces

--- solutions/devsprint-leonardo-santos-3/FinanceAppTests/Services/Entities/ActivityTests.swift
+++ solutions/devsprint-leonardo-santos-3/FinanceAppTests/Services/Entities/ActivityTests.swift
@@ -1,1 +1,15 @@
+import XCTest
 
+@testable import FinanceApp
+
+final class ActivityTests: XCTestCase {
+    private let sut = Activity(name: "Atividade de teste", price: 2, time: "1")
+    
+    func test_formattedInfo_shouldReturnFormattedActivityInfo(){
+        let expectedFormattedInfo = sut.formattedInfo()
+        
+        XCTAssertNotNil(expectedFormattedInfo)
+        XCTAssertEqual(expectedFormattedInfo, "$\(sut.price) • \(sut.time)")
+    }
+    
+}
