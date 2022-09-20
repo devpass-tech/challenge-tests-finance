29b075d06eaa6e3e6ef742a5334177bb980ffc19
Pedro Gabriel <pee.torres@gmail.com>
2022-09-20 09:31:48 +0100

    Implement test fetch ActivityDetailsSuccess

--- solutions/devsprint-leonardo-santos-3/FinanceAppTests/Services/FinanceServiceTests.swift
+++ solutions/devsprint-leonardo-santos-3/FinanceAppTests/Services/FinanceServiceTests.swift
@@ -1,1 +1,33 @@
 
+import XCTest
+
+@testable import FinanceApp
+
+class FinanceServiceTests: XCTestCase {
+
+    func test_fetch_ActivityDetailsSuccess() {
+        let expectation = expectation(description: "Waiting request")
+        let networkClientSpy = NetworkClientSpy()
+        let givenJSON = """
+        {
+          "name": "Mall",
+          "category": "Shopping",
+          "price": 100.0,
+          "time": "8:57 AM"
+        }
+        """
+        networkClientSpy.completionData = givenJSON.data(using: .utf8)
+        let sut = FinanceService(networkClient: networkClientSpy)
+
+        let expectedResult = ActivityDetails(name: "Mall",
+                                             price: 100.0,
+                                             category: "Shopping",
+                                             time: "8:57 AM")
+
+        sut.fetchActivityDetails { response in
+            XCTAssertEqual(response, expectedResult)
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 1)
+    }
+}
