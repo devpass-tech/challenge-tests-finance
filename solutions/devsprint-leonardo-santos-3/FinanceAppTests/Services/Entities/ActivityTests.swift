5a7e6c70584441a3ca42a99494391004ebb4602a
Luiza Avelino <avelino.luiza98@gmail.com>
2022-09-20 11:04:58 -0300

    add: formattedInfo tests

--- solutions/devsprint-leonardo-santos-3/FinanceAppTests/Services/Entities/ActivityTests.swift
+++ solutions/devsprint-leonardo-santos-3/FinanceAppTests/Services/Entities/ActivityTests.swift
@@ -1,1 +1,17 @@
+import XCTest
 
+@testable import FinanceApp
+
+final class ActivityTests:XCTestCase {
+    let sut = Activity(name: "Atividade de teste", price: 2, time: "1")
+    
+    
+    func test_formattedInfo_shouldReturnFormattedActivityInfo(){
+        let formattedInfo = sut.formattedInfo()
+        
+        
+        XCTAssertNotNil(formattedInfo)
+        XCTAssertEqual(formattedInfo, "$\(sut.price) • \(sut.time)")
+    }
+    
+}
