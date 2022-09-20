20d64459e0945abdfc4a5a3e6b6d2cb025dfa844
Brun41v35 <alves.bruno945@gmail.com>
2022-09-19 23:39:38 -0300

    Implementing test in formattedAccount method

--- solutions/devsprint-leonardo-santos-3/FinanceAppTests/Services/Entities/AccountTests.swift
+++ solutions/devsprint-leonardo-santos-3/FinanceAppTests/Services/Entities/AccountTests.swift
@@ -1,1 +1,20 @@
+import XCTest
+@testable import FinanceApp
 
+final class AccountTests: XCTestCase {
+    
+    // MARK: - Private Properties
+    
+    private let sut = Account(agency: "1111", account: "000011")
+    
+    // MARK: - Methods
+    
+    func test_formattedAccount_shouldReturnFormattedAccount() {
+        
+        // When
+        let formattedAccount = sut.formattedAccount()
+        
+        // Then
+        XCTAssertEqual(formattedAccount, "Account: \(sut.account)")
+    }
+}
