016901bb8778d3bc425eb80895d6c0b2d588765b
Luiza Avelino <luizaa@ciandt.com>
2022-09-20 10:52:39 -0300

    add: inside navigation controller tests

--- solutions/devsprint-leonardo-santos-3/FinanceAppTests/Extensions/UIViewControllerExtensionsTests.swift
+++ solutions/devsprint-leonardo-santos-3/FinanceAppTests/Extensions/UIViewControllerExtensionsTests.swift
@@ -1,1 +1,20 @@
+import XCTest
 
+@testable import FinanceApp
+
+
+final class UIViewControllerExtensionsTests:XCTestCase {
+    private let sut = UIViewController()
+    
+    
+    func test_insideNavigationController_shouldReturnNavigationController(){
+        
+        let navigationController =  sut.insideNavigationController()
+        
+        
+        XCTAssertNotNil(navigationController)
+        XCTAssertEqual(navigationController.topViewController, sut)
+        XCTAssertEqual(navigationController.modalPresentationStyle, .formSheet)
+        
+    }
+}
