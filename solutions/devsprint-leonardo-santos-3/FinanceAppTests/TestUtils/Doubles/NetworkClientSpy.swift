5b252000c1351b21d24956b3c30adcdeef54ca5a
Pedro Gabriel <pee.torres@gmail.com>
2022-09-20 09:55:05 +0100

    Update URL property into NetworkClientSpy

--- solutions/devsprint-leonardo-santos-3/FinanceAppTests/TestUtils/Doubles/NetworkClientSpy.swift
+++ solutions/devsprint-leonardo-santos-3/FinanceAppTests/TestUtils/Doubles/NetworkClientSpy.swift
@@ -4,11 +4,13 @@ import Foundation
 
 class NetworkClientSpy: NetworkClientProtocol {
     // MARK: Properties
+    var url: URL?
     var completionData: Data?
     var performRequestCount: Int = 0
 
     // MARK: Methods
     func performRequest(with url: URL, completion: @escaping (Data?) -> ()) {
+        self.url = url
         performRequestCount += 1
         completion(completionData)
     }
