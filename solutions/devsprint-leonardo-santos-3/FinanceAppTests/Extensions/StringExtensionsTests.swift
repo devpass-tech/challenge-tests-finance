ca2bb4631b7275221a6e1f91a7bd32c22a7dc110
Giuliano <giulianoaccorsi@gmail.com>
2022-09-21 18:30:37 -0300

    Add StringExtensionsTests

--- solutions/devsprint-leonardo-santos-3/FinanceAppTests/Extensions/StringExtensionsTests.swift
+++ solutions/devsprint-leonardo-santos-3/FinanceAppTests/Extensions/StringExtensionsTests.swift
@@ -1,1 +1,15 @@
+import XCTest
 
+@testable import FinanceApp
+
+final class StringExtensionsTests: XCTestCase {
+    private var sut = String()
+
+    func test_string_shouldRemoveSpaces() {
+        sut = "  teste"
+        let stringWithoutSpaces = sut.removingSpaces()
+
+        XCTAssertEqual(stringWithoutSpaces, "teste")
+    }
+
+}
