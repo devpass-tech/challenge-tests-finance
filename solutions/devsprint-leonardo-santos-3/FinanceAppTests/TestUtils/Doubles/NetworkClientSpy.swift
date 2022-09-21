8da0e8e3680ec97b54a747fe3254f2aa242c21ef
Pedro Gabriel <pee.torres@gmail.com>
2022-09-21 22:42:46 +0100

    ✨: Create NetworkClientSpy (#161)
    * Create NetworkClientSpy
    
    * Update URL property into NetworkClientSpy
    
    * Fix MR suggestions

--- solutions/devsprint-leonardo-santos-3/FinanceAppTests/TestUtils/Doubles/NetworkClientSpy.swift
+++ solutions/devsprint-leonardo-santos-3/FinanceAppTests/TestUtils/Doubles/NetworkClientSpy.swift
@@ -1,1 +1,17 @@
+import Foundation
 
+@testable import FinanceApp
+
+final class NetworkClientSpy: NetworkClientProtocol {
+    // MARK: Properties
+    var completionData: Data?
+    private(set) var url: URL?
+    private(set)var performRequestCount: Int = 0
+
+    // MARK: Methods
+    func performRequest(with url: URL, completion: @escaping (Data?) -> ()) {
+        self.url = url
+        performRequestCount += 1
+        completion(completionData)
+    }
+}
