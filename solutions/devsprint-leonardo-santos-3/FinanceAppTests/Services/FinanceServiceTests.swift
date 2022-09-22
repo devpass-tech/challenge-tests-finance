2f9364ea33910f4c99fc617a3b4a7e392621d7d0
Giuliano Accorsi <giulianoaccorsi@gmail.com>
2022-09-22 15:17:43 -0300

    add UserProfile+Fixture

--- solutions/devsprint-leonardo-santos-3/FinanceAppTests/Services/FinanceServiceTests.swift
+++ solutions/devsprint-leonardo-santos-3/FinanceAppTests/Services/FinanceServiceTests.swift
@@ -1,1 +1,66 @@
 
+import XCTest
+
+@testable import FinanceApp
+
+final class FinanceServiceTests: XCTestCase {
+    private let networkClientSpy = NetworkClientSpy()
+    private lazy var sut = FinanceService(networkClient: networkClientSpy)
+    private let correctData =
+    """
+    {
+        "name": "Irma Flores",
+        "phone": "+55 (11) 99999-9999",
+        "email": "irma@devpass.com.br",
+        "address": "Rua Bela Cintra, 495",
+        "account": {
+            "agency": "0001",
+            "account": "123456-7"
+        }
+    }
+    """.data(using: .utf8)
+
+    private let incorrectData =
+    """
+    """.data(using: .utf8)
+    
+    func test_fetchUserProfile_shouldSendCorrectURL() {
+        sut.fetchUserProfile { _ in }
+        XCTAssertEqual(networkClientSpy.performRequestCount, 1)
+        XCTAssertEqual(networkClientSpy.url?.description, "https://raw.githubusercontent.com/devpass-tech/challenge-finance-app/main/api/user_profile_endpoint.json")
+    }
+    
+    func test_fetchUserProfile_givenNilData_shouldSendNil() {
+        networkClientSpy.completionData = nil
+        var expectedData: UserProfile?
+
+        sut.fetchUserProfile { data in
+            expectedData = data
+        }
+
+        XCTAssertEqual(networkClientSpy.performRequestCount, 1)
+        XCTAssertNil(expectedData)
+    }
+
+    func test_fetchUserProfile_givenData_shouldReturnCorrectUserProfile() {
+        networkClientSpy.completionData = correctData
+        var expectedData: UserProfile?
+
+        sut.fetchUserProfile { data in
+            expectedData = data
+        }
+
+        XCTAssertEqual(expectedData, .fixture(name: "Irma Flores", phone: "+55 (11) 99999-9999", email: "irma@devpass.com.br", address: "Rua Bela Cintra, 495", agency: "0001", account: "123456-7"))
+    }
+
+    func test_fetchUserProfile_givenDataWithFailureParse_shouldReturnCorrectUserProfile() {
+        networkClientSpy.completionData = incorrectData
+        var expectedData: UserProfile?
+
+        sut.fetchUserProfile { data in
+            expectedData = data
+        }
+
+        XCTAssertNil(expectedData)
+    }
+}
