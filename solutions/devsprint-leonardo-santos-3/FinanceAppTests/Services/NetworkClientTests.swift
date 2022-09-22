8e06b274c692620b8221efe94a989c2141f2878b
Douglas Garcia <douglas.garcia@ymail.com>
2022-09-21 21:30:06 -0300

    NetworkClient tests

--- solutions/devsprint-leonardo-santos-3/FinanceAppTests/Services/NetworkClientTests.swift
+++ solutions/devsprint-leonardo-santos-3/FinanceAppTests/Services/NetworkClientTests.swift
@@ -1,1 +1,88 @@
 
+import XCTest
+@testable import FinanceApp
+
+final class NetworkClientTests: XCTestCase {
+    
+    // MARK: - Private Properties
+    
+    private let spy = NetworkClientSpy()
+    
+    // MARK: - Methods
+    
+    func test_performRequest_shouldBeCalled() throws {
+        
+        // When
+        spy.performRequest(with: .fixtureURL, completion: { _ in })
+        
+        // Then
+        XCTAssertTrue(spy.performRequestCalled)
+    }
+    
+    func test_performRequest_shouldBeCalledOnceOnly() throws {
+        
+        // When
+        spy.performRequest(with: .fixtureURL, completion: { _ in })
+        
+        // Then
+        XCTAssertEqual(spy.performRequestCount, 1)
+    }
+    
+    func test_performRequest_givenNotNil_shouldReturnURL() throws {
+        
+        // When
+        spy.performRequest(with: .fixtureURL, completion: { _ in })
+        
+        // Then
+        XCTAssertNotNil(spy.url)
+    }
+    
+    func test_performRequest_shouldReturnURL() throws {
+        
+        // When
+        spy.performRequest(with: .fixtureURL, completion: { _ in })
+        
+        // Then
+        XCTAssertEqual(spy.url, .fixtureURL)
+    }
+    
+    func test_perfomRequest_givenData_shouldBeCompletionDataSuccessfully() throws {
+
+        var expectedData: Data?
+        
+        // When
+        spy.completionData = .fixtureData
+        spy.performRequest(with: .fixtureURL, completion: {
+            expectedData = $0
+        })
+
+        // Then
+        XCTAssertEqual(expectedData, .fixtureData)
+    }
+    
+    func test_perfomRequest_givenNilData_shouldBeCompletionData() throws {
+        
+        var expectedData: Data?
+
+        // When
+        spy.performRequest(with: .fixtureURL, completion: {
+            expectedData = $0
+        })
+
+        // Then
+        XCTAssertNil(expectedData)
+    }
+}
+
+
+extension URL {
+    static var fixtureURL: Self {
+        URL(fileURLWithPath: "some/path")
+    }
+}
+
+extension Data {
+    static var fixtureData: Self? {
+        "".data(using: .utf8)
+    }
+}
--- solutions/devsprint-leonardo-santos-3/FinanceAppTests/TestUtils/Doubles/NetworkClientSpy.swift
+++ solutions/devsprint-leonardo-santos-3/FinanceAppTests/TestUtils/Doubles/NetworkClientSpy.swift
@@ -3,15 +3,20 @@ import Foundation
 @testable import FinanceApp
 
 final class NetworkClientSpy: NetworkClientProtocol {
+    
     // MARK: Properties
+    
     var completionData: Data?
     private(set) var url: URL?
-    private(set)var performRequestCount: Int = 0
+    private(set) var performRequestCount: Int = 0
+    private(set) var performRequestCalled = false
 
     // MARK: Methods
+    
     func performRequest(with url: URL, completion: @escaping (Data?) -> ()) {
         self.url = url
         performRequestCount += 1
+        performRequestCalled = true
         completion(completionData)
     }
 }
