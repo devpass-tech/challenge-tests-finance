4a8f3140c78ad42e1a22c79a35b6b7da15c12bc8
Rafael Benjamin <rafaelb.90@hotmail.com>
2022-09-22 18:39:03 -0300

    fixing merge

--- solutions/devsprint-leonardo-santos-3/FinanceAppTests/Services/FinanceServiceTests.swift
+++ solutions/devsprint-leonardo-santos-3/FinanceAppTests/Services/FinanceServiceTests.swift
@@ -44,7 +44,72 @@ final class FinanceServiceTests: XCTestCase {
 
         makeDefaultTests()
     }
+    
+    func test_transferAmount_wasCalled(){
+        sut.transferAmount { _ in  }
+        
+        XCTAssertTrue(networkClientSpy.performRequestCalled)
+    }
+    
+    func test_transferAmount_wasCalledOnlyOnce(){
+        sut.transferAmount { _ in  }
+        
+        XCTAssertEqual(networkClientSpy.performRequestCount, 1)
+    }
+    
+    func test_transferAmount_shouldSendCorrectUrl(){
+        sut.transferAmount { _ in  }
+        
+        XCTAssertEqual(networkClientSpy.url?.description, "https://raw.githubusercontent.com/devpass-tech/challenge-finance-app/main/api/transfer_successful_endpoint.json")
+    }
+    
+    func test_transferAmount_whenPerformRequestDataReturnNil_shouldReturnNil(){
+        var expectedData: TransferResult?
+        
+        sut.transferAmount { expectedData = $0 }
+        
+        XCTAssertEqual(networkClientSpy.completionData, nil)
+        XCTAssertEqual(expectedData, nil)
+    }
+    
+    func test_transferAmount_whenPerformRequestDataReturnValidData_shouldReturnTransferData(){
+        
+        var transferDataJSON: Data? {
+            """
+            {
+                "success": true
+            }
+            """.data(using: .utf8)
+        }
+        
+        networkClientSpy.completionData = transferDataJSON
+        
+        sut.transferAmount {
+            XCTAssertEqual($0, .fixture(success: true))
+        }
+        
+    }
+    
+    func test_transferAmount_givenData_withFailedParse_shouldReturnNil(){
+        
+        var transferDataJSON: Data? {
+            """
+            {
+                "_": "false"
+            }
+            """.data(using: .utf8)
+        }
+        
+        networkClientSpy.completionData = transferDataJSON
+        
+        sut.transferAmount {
+            XCTAssertNil($0)
+        }
+        
+    }
 }
+
+
 
 extension FinanceServiceTests {
 
