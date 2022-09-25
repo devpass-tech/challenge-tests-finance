24df93ff2ccd7c401cbf6d2fc360e143222acaf0
Pedro Gabriel <pee.torres@gmail.com>
2022-09-26 00:25:23 +0100

    Update ActivityDetailViewModelTests of fetchData_calledDispatchQueueAsync

--- solutions/devsprint-leonardo-santos-3/FinanceAppTests/Modules/ActivityDetails/ActivityDetailsViewModelTests.swift
+++ solutions/devsprint-leonardo-santos-3/FinanceAppTests/Modules/ActivityDetails/ActivityDetailsViewModelTests.swift
@@ -1,1 +1,34 @@
+import XCTest
 
+@testable import FinanceApp
+
+final class ActivityDetailViewModelTests: XCTestCase {
+
+    func test_fetchData_calledDispatchQueueAsync() {
+        let networkClientSpy = NetworkClientSpy()
+        let financeServiceSpy = FinanceService(networkClient: networkClientSpy)
+        let dispatchQueueSpy = DispatchQueueSpy()
+        let sut = ActivityDetailsViewModel(financeService: financeServiceSpy,
+                                           dispatchQueue: dispatchQueueSpy)
+
+        
+        networkClientSpy.completionData = correctActivityDetailsData
+        sut.fetchData()
+
+        XCTAssertTrue(dispatchQueueSpy.calledDispatchQueueAsync)
+    }
+
+}
+
+extension ActivityDetailViewModelTests {
+    var correctActivityDetailsData: Data? {
+        """
+        {
+          "name": "Mall",
+          "category": "Shopping",
+          "price": 100.0,
+          "time": "8:57 AM"
+        }
+        """.data(using: .utf8)
+    }
+}
