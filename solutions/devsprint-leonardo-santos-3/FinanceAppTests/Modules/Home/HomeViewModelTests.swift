8fdebc7b9f6963c26e1f093f1cf3d3ecb284d103
Pedro Gabriel <pee.torres@gmail.com>
2022-09-25 23:56:17 +0100

    Start implementation of DispatchQueueSpy

--- solutions/devsprint-leonardo-santos-3/FinanceAppTests/Modules/Home/HomeViewModelTests.swift
+++ solutions/devsprint-leonardo-santos-3/FinanceAppTests/Modules/Home/HomeViewModelTests.swift
@@ -1,1 +1,21 @@
+import XCTest
+
+@testable import FinanceApp
+
+final class HomeViewModelTests: XCTestCase {
+
+    func test_fetchData_calledDispatchQueueAsync() {
+        let networkClientSpy = NetworkClientSpy()
+        let financeServiceSpy = FinanceService(networkClient: networkClientSpy)
+        let dispatchQueueSpy = DispatchQueueSpy()
+        let sut = HomeViewModel(financeService: financeServiceSpy,
+                                dispatchQueue: dispatchQueueSpy)
+
+        networkClientSpy.completionData = HomeData(balance: 12, savings: 12, spending: 12, activity: [])
+        sut.fetchData()
+
+        XCTAssertTrue(dispatchQueueSpy.calledDispatchQueueAsync)
+    }
+
+}
 
--- solutions/devsprint-leonardo-santos-3/FinanceAppTests/TestUtils/Doubles/DispatchQueueSpy.swift
+++ solutions/devsprint-leonardo-santos-3/FinanceAppTests/TestUtils/Doubles/DispatchQueueSpy.swift
@@ -1,1 +1,27 @@
+import Foundation
+@testable import FinanceApp
 
+class DispatchQueueSpy: DispatchQueueProtocol {
+    // MARK: Properties
+    var deadlinePassed: DispatchTime?
+    var calledDispatchQueueAsync: Bool = false
+    var calledDispatchQueueAsyncAfter: Bool = false
+
+    // MARK: Methods
+    func async(group: DispatchGroup? = nil,
+               qos: DispatchQoS = .unspecified,
+               flags: DispatchWorkItemFlags = [],
+               execute work: @escaping @convention(block) () -> Void) {
+        calledDispatchQueueAsync = true
+        work()
+    }
+
+    func asyncAfter(deadline: DispatchTime,
+                    qos: DispatchQoS = .unspecified,
+                    flags: DispatchWorkItemFlags = [],
+                    execute work: @escaping @convention(block) () -> Void) {
+        deadlinePassed = deadline
+        calledDispatchQueueAsyncAfter = true
+        work()
+    }
+}
