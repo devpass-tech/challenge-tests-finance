ce5d4874f2be033364b9b3033f349bfad24fb7f7
Luiza Avelino <avelino.luiza98@gmail.com>
2022-09-26 15:25:00 -0300

    create HomeViewModelTests

--- solutions/devsprint-leonardo-santos-3/FinanceAppTests/Modules/Home/HomeViewModelTests.swift
+++ solutions/devsprint-leonardo-santos-3/FinanceAppTests/Modules/Home/HomeViewModelTests.swift
@@ -1,1 +1,38 @@
+import XCTest
 
+@testable import FinanceApp
+
+final class HomeViewModelTests: XCTestCase {
+    private let delegateSpy = HomeViewModelDelegateSpy()
+    private let financeSpy = FinanceServiceSpy()
+    private let dispatchQueueSpy = DispatchQueueSpy()
+    
+    private lazy var sut = HomeViewModel(
+            financeService: financeSpy,
+            dispatchQueue: dispatchQueueSpy
+    )
+    
+    func test_fetchData_givenNilData_shouldNotCallDelegate(){
+        financeSpy.fetchHomeDataCompletionDataToBeReturned = nil
+        
+        sut.fetchData()
+        
+        XCTAssertTrue(financeSpy.fetchHomeDataCalled)
+        XCTAssertNil(financeSpy.fetchHomeDataCompletionDataToBeReturned)
+        XCTAssertFalse(dispatchQueueSpy.asyncCalled)
+        XCTAssertFalse(delegateSpy.didFetchHomeDataCalled)
+    }
+    
+    func test_fetchData_givenValidaData_shouldCallDelegate(){
+        sut.delegate = delegateSpy
+        
+        financeSpy.fetchHomeDataCompletionDataToBeReturned = HomeData.fixture(balance: 0, savings: 0, spending: 0, activity: [])
+        
+        sut.fetchData()
+        
+        XCTAssertTrue(financeSpy.fetchHomeDataCalled)
+        XCTAssertNotNil(financeSpy.fetchHomeDataCompletionDataToBeReturned)
+        XCTAssertTrue(dispatchQueueSpy.asyncCalled)
+        XCTAssertTrue(delegateSpy.didFetchHomeDataCalled)
+    }
+}
--- solutions/devsprint-leonardo-santos-3/FinanceAppTests/TestUtils/Doubles/DispatchQueueSpy.swift
+++ solutions/devsprint-leonardo-santos-3/FinanceAppTests/TestUtils/Doubles/DispatchQueueSpy.swift
@@ -1,1 +1,13 @@
+import XCTest
 
+@testable import FinanceApp
+
+final class DispatchQueueSpy: DispatchQueueProtocol {
+    
+    private(set) var asyncCalled: Bool = false
+    
+    func async(group: DispatchGroup?, qos: DispatchQoS, flags: DispatchWorkItemFlags, execute work: @escaping @convention(block) () -> Void) {
+        asyncCalled = true
+        work()
+    }
+}
