06d3fccaac6d50014d120742bf36daaf70cdbb35
Luiza Avelino <avelino.luiza98@gmail.com>
2022-09-26 10:40:13 -0300

    add Activity & HomeData fixttures

---
+++ solutions/devsprint-leonardo-santos-3/FinanceAppTests/TestUtils/Fixtures/HomeData+Fixture.swift
@@ -0,0 +1,13 @@
+@testable import FinanceApp
+
+extension HomeData {
+    static func fixture(balance: Float = 0.0,
+                        savings: Float = 0.0,
+                        spending: Float = 0.0,
+                        activity: [Activity] = []) -> Self {
+        .init(balance: balance,
+              savings: savings,
+              spending: spending,
+              activity: activity)
+    }
+}
