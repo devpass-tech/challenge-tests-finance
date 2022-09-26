2af0162c9618ce0a61ed94c8c88aac1c43bdb73f
Luiza Avelino <avelino.luiza98@gmail.com>
2022-09-26 15:23:54 -0300

    add FinanceServiceSpy

--- solutions/devsprint-leonardo-santos-3/FinanceAppTests/TestUtils/Doubles/FinanceServiceSpy.swift
+++ solutions/devsprint-leonardo-santos-3/FinanceAppTests/TestUtils/Doubles/FinanceServiceSpy.swift
@@ -1,8 +1,23 @@
-//
-//  FinanceServiceSpy.swift
-//  FinanceAppTests
-//
-//  Created by Luiza Rodrigues de Almeida Avelino on 26/09/22.
-//
-
 import Foundation
+
+@testable import FinanceApp
+
+final class FinanceServiceSpy: FinanceServiceProtocol {
+    var fetchHomeDataCalled: Bool = false
+    var fetchHomeDataCompletionDataToBeReturned: HomeData?
+    
+    func fetchHomeData(_ completion: @escaping (HomeData?) -> Void) {
+        fetchHomeDataCalled = true
+        
+        completion(fetchHomeDataCompletionDataToBeReturned)
+    }
+    
+    func fetchActivityDetails(_ completion: @escaping (ActivityDetails?) -> Void) {}
+    
+    func fetchContactList(_ completion: @escaping ([Contact]?) -> Void) {}
+    
+    func fetchUserProfile(_ completion: @escaping (UserProfile?) -> Void) {}
+    
+    func transferAmount(_ completion: @escaping (TransferResult?) -> Void) {}
+    
+}
