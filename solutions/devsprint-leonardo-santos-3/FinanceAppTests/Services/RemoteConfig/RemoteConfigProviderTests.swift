b17de8a211184151e0c3a7143a483134951a9d65
Rafael Benjamin <rafaelb.90@hotmail.com>
2022-09-27 20:56:05 -0300

    including RemoteConfigProviderTests

--- solutions/devsprint-leonardo-santos-3/FinanceAppTests/Services/RemoteConfig/RemoteConfigProviderTests.swift
+++ solutions/devsprint-leonardo-santos-3/FinanceAppTests/Services/RemoteConfig/RemoteConfigProviderTests.swift
@@ -1,1 +1,33 @@
+import XCTest
+@testable import FinanceApp
 
+final class RemoteConfigProviderTests: XCTestCase {
+    
+    private let sut = RemoteConfigProvider()
+    private let feature = RemoteConfigProvider.Feature.self
+    
+    func test_shouldExecute_givenFeatureGetDefaultProfileFeature(){
+        let expectedResult = sut.shouldExecute(feature.getDefaultProfile)
+        
+        XCTAssertTrue(expectedResult)
+    }
+    
+    func test_shouldExecute_givenFeatureGetProfileTypePremium(){
+        let expectedResult = sut.shouldExecute(feature.getProfileType(feature.ProfileType.premium))
+        
+        XCTAssertTrue(expectedResult)
+    }
+    
+    func test_shouldExecute_givenFeatureGetProfileTypeStandard(){
+        let expectedResult = sut.shouldExecute(feature.getProfileType(feature.ProfileType.standard))
+        
+        XCTAssertFalse(expectedResult)
+    }
+    
+    func test_shouldExecute_givenFeatureGetActivities(){
+        let expectedResult = sut.shouldExecute(feature.getActivities)
+        
+        XCTAssertTrue(expectedResult)
+    }
+    
+}
