a84ebf1c7439b2acc174e49df77b53f5e553506c
Rafael Benjamin <rafaelb.90@hotmail.com>
2022-09-29 18:41:24 -0300

    Remoteconfigprovider tests (#177)
    * including TabBarControllerTests
    
    * updating assertion on tabbarcontroller tests
    
    * including RemoteConfigProviderTests
    
    * removing unused classes
    
    * renaming remoteconfigprovider tests

--- solutions/devsprint-leonardo-santos-3/FinanceAppTests/Services/RemoteConfig/RemoteConfigProviderTests.swift
+++ solutions/devsprint-leonardo-santos-3/FinanceAppTests/Services/RemoteConfig/RemoteConfigProviderTests.swift
@@ -1,1 +1,32 @@
+import XCTest
+@testable import FinanceApp
 
+final class RemoteConfigProviderTests: XCTestCase {
+    
+    private let sut = RemoteConfigProvider()
+    
+    func test_shouldExecute_givenDefaultProfile_shouldReturnTrue(){
+        let expectedResult = sut.shouldExecute(.getDefaultProfile)
+        
+        XCTAssertTrue(expectedResult)
+    }
+    
+    func test_shouldExecute_givenProfileTypePremium_shouldReturnTrue(){
+        let expectedResult = sut.shouldExecute(.getProfileType(.premium))
+        
+        XCTAssertTrue(expectedResult)
+    }
+    
+    func test_shouldExecute_givenProfileTypeStandard_shouldReturnFalse(){
+        let expectedResult = sut.shouldExecute(.getProfileType(.standard))
+        
+        XCTAssertFalse(expectedResult)
+    }
+    
+    func test_shouldExecute_givenActivities_shouldReturnTrue(){
+        let expectedResult = sut.shouldExecute(.getActivities)
+        
+        XCTAssertTrue(expectedResult)
+    }
+    
+}
