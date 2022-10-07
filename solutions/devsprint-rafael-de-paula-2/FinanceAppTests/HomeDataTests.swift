0db8b88a341aa491d3aaaa0dfc91cebdb0c7a461
Márcio Habigzang Brufatto <mhbrufatto@gmail.com>
2022-10-06 23:55:39 -0300

    Created test for formatted info method.

--- solutions/devsprint-rafael-de-paula-2/FinanceAppTests/HomeDataTests.swift
+++ solutions/devsprint-rafael-de-paula-2/FinanceAppTests/HomeDataTests.swift
@@ -9,5 +9,16 @@ import XCTest
 @testable import FinanceApp
 
 class HomeDataTests: XCTestCase {
-
+    
+    func test_formattedInfo_whenActivityHasData_shouldRetrunsFormattedString() {
+        let activity = Activity(name: "Person", price: 300, time: "13/12/2022 - 12:00")
+        let expectedResult = "$\(activity.price) • \(activity.time)"
+        XCTAssertEqual(activity.formattedInfo(), expectedResult)
+    }
+    
+    func test_formattedInfo_whenPriceZeroAndTimeAreEmpty_shouldRetrunsFormattedString() {
+        let activity = Activity(name: "Person", price: 0, time: "")
+        let expectedResult = "$\(activity.price) • \(activity.time)"
+        XCTAssertEqual(activity.formattedInfo(), expectedResult)
+    }
 }
