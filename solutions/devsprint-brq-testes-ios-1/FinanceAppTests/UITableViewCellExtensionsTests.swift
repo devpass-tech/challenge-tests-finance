152de433ca9117036bf8723536765f1eef6fd68c
Marcos Amorim <marcos.amorim.rc@gmail.com>
2022-11-10 14:11:38 -0300

    teste para método classIdentifier

--- solutions/devsprint-brq-testes-ios-1/FinanceAppTests/UITableViewCellExtensionsTests.swift
+++ solutions/devsprint-brq-testes-ios-1/FinanceAppTests/UITableViewCellExtensionsTests.swift
@@ -2,5 +2,10 @@ import XCTest
 @testable import FinanceApp
 
 class UITableViewCellExtensionsTests: XCTestCase {
+    
+//    private let sut = ActivityCellView()
 
+    func test_classIdentifier_shoudReturnClassName() {
+        XCTAssertEqual(ActivityCellView.classIdentifier(), "ActivityCellView")
+    }
 }
