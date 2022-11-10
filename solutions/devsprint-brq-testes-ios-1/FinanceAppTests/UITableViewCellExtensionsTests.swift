345a24445f1ab49138c0a7c3a563cdb7fdbb9918
Marcos Amorim <marcos.amorim.rc@gmail.com>
2022-11-10 18:09:50 -0300

    remoção de comentario

--- solutions/devsprint-brq-testes-ios-1/FinanceAppTests/UITableViewCellExtensionsTests.swift
+++ solutions/devsprint-brq-testes-ios-1/FinanceAppTests/UITableViewCellExtensionsTests.swift
@@ -2,8 +2,6 @@ import XCTest
 @testable import FinanceApp
 
 class UITableViewCellExtensionsTests: XCTestCase {
-    
-//    private let sut = ActivityCellView()
 
     func test_classIdentifier_shoudReturnClassName() {
         XCTAssertEqual(ActivityCellView.classIdentifier(), "ActivityCellView")
