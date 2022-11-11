2727ad0a47b3b6bf3b5ab676bbfc64b3ebb31e77
Marcos Amorim <marcos.amorim.rc@gmail.com>
2022-11-11 15:12:12 -0300

    Criação do NetworkClientSpy

--- solutions/devsprint-brq-testes-ios-1/FinanceAppTests/Test Doubles/NetworkClientSpy.swift
+++ solutions/devsprint-brq-testes-ios-1/FinanceAppTests/Test Doubles/NetworkClientSpy.swift
@@ -1,1 +1,20 @@
 import Foundation
+
+@testable import FinanceApp
+
+class NetWorkClientSpy: NetworkClientProtocol {
+    
+    
+    private(set) var performRequestCalled: Bool = false
+    private(set) var performRequestCallCount: Int = 0
+    private(set) var performRequestURL: URL?
+    var performRequestCompletionDataToBeReturned: Data?
+
+    func performRequest(with url: URL, completion: @escaping (Data?) -> ()) {
+        performRequestCalled = true
+        performRequestCallCount += 1
+        performRequestURL = url
+        completion(performRequestCompletionDataToBeReturned)
+    }
+    
+}
