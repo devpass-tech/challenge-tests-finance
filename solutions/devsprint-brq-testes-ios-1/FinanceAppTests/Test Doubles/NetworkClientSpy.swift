823da76b654332a096d337904d7fd97068c8a06c
MarcosAmorimBRQ <117674243+MarcosAmorimBRQ@users.noreply.github.com>
2022-11-14 14:16:52 -0300

    Criação do NetworkClientSpy (#193)
    * Criação do NetworkClientSpy
    
    * Update solutions/devsprint-brq-testes-ios-1/FinanceAppTests/Test Doubles/NetworkClientSpy.swift
    
    Co-authored-by: Leonardo <42387881+leocoout@users.noreply.github.com>
    
    * Update solutions/devsprint-brq-testes-ios-1/FinanceAppTests/Test Doubles/NetworkClientSpy.swift
    
    Co-authored-by: Leonardo <42387881+leocoout@users.noreply.github.com>
    
    Co-authored-by: Marcos Amorim <marcos.amorim.rc@gmail.com>
    Co-authored-by: Leonardo <42387881+leocoout@users.noreply.github.com>

--- solutions/devsprint-brq-testes-ios-1/FinanceAppTests/Test Doubles/NetworkClientSpy.swift
+++ solutions/devsprint-brq-testes-ios-1/FinanceAppTests/Test Doubles/NetworkClientSpy.swift
@@ -1,1 +1,19 @@
 import Foundation
+
+@testable import FinanceApp
+
+final class NetworkClientSpy: NetworkClientProtocol {
+    
+    private(set) var performRequestCalled: Bool = false
+    private(set) var performRequestCallCount: Int = 0
+    private(set) var performRequestURL: URL?
+    var performRequestCompletionDataToBeReturned: Data?
+
+    func performRequest(with url: URL, completion: @escaping (Data?) -> ()) {
+        performRequestCalled = true
+        performRequestCallCount += 1
+        performRequestURL = url
+        completion(performRequestCompletionDataToBeReturned)
+    }
+    
+}
