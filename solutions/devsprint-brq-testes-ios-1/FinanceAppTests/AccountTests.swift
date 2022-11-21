8f030a6dea99c6077fc5cd7555ffe506bebab8f9
Cassia Franco <cassia.developer@gmail.com>
2022-11-21 14:09:27 -0300

    Feature/Tests Agency  (#194)
    * [US] #Teste formattedAgency
    
    * Update solutions/devsprint-brq-testes-ios-1/FinanceAppTests/AccountTests.swift
    
    Change the name metodo
    
    Co-authored-by: Leonardo <42387881+leocoout@users.noreply.github.com>
    
    Co-authored-by: Cassia <cassiafranco@brq.com>
    Co-authored-by: Leonardo <42387881+leocoout@users.noreply.github.com>

--- solutions/devsprint-brq-testes-ios-1/FinanceAppTests/AccountTests.swift
+++ solutions/devsprint-brq-testes-ios-1/FinanceAppTests/AccountTests.swift
@@ -1,1 +1,14 @@
-import Foundation
+
+import XCTest
+@testable import FinanceApp
+
+final class AccountTests: XCTestCase {
+        
+    func test_formattedAccount_shouldReturnCorrectFormattedAccount() {
+        let account = Account(agency: "Agency 0007", account: "")
+        let result = "Account: \(account.account)"
+       
+        XCTAssertEqual(account.formattedAccount(), result)
+        
+    }
+}
